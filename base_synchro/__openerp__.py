6f3eafa36c7547e2ade5ce550a3ea3443278d43a
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2014-09-18 19:06:11 +0530

    Added base_module_record and base_synchro

---
+++ base_synchro/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2013 Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Multi-DB Synchronization",
+    "version": "1.1",
+    "category": "Tools",
+    "description": """
+Synchronization with all objects.
+=================================
+
+Configure servers and trigger synchronization with its database objects.
+""",
+    "author": "OpenERP SA, Serpent Consulting Services Pvt. Ltd.",
+    "images": ['images/1_servers_synchro.jpeg','images/2_synchronize.jpeg','images/3_objects_synchro.jpeg',],
+    "depends": ["base"],
+    'website': 'http://www.serpentcs.com',
+    "data": [
+        "wizard/base_synchro_view.xml",
+        "base_synchro_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
