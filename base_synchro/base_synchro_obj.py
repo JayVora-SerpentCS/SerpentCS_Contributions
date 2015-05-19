1a3df56163058299147f26366b00d374e9c13d08
khushboo <k.bhatt.serpentcs@gmail.com>
2015-05-19 14:49:18 +0530

    [IMP]Implemented code as per odoo API

--- base_synchro/base_synchro_obj.py
+++ base_synchro/base_synchro_obj.py
@@ -20,88 +20,82 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
+from openerp import models, fields, api, _
+
 import time
 
-class base_synchro_server(osv.Model):
+class base_synchro_server(models.Model):
     '''Class to store the information regarding server'''
     _name = "base.synchro.server"
     _description = "Synchronized server"
-    _columns = {
-        'name': fields.char('Server name', size=64, required=True),
-        'server_url': fields.char('Server URL', size=64, required=True),
-        'server_port': fields.integer('Server Port', size=64, required=True),
-        'server_db': fields.char('Server Database', size=64, required=True),
-        'login': fields.char('User Name', size=50, required=True),
-        'password': fields.char('Password', size=64, required=True),
-        'obj_ids' : fields.one2many('base.synchro.obj', 'server_id', 'Models', ondelete='cascade')
-    }
-    _defaults = {
-        'server_port': lambda *args: 8069
-    }
 
-class base_synchro_obj(osv.Model):
+    name = fields.Char(string='Server name', size=64, required=True)
+    server_url = fields.Char(string='Server URL', size=64, required=True)
+    server_port = fields.Integer(string='Server Port', size=64, required=True, default=8069)
+    server_db = fields.Char(string='Server Database', size=64, required=True)
+    login = fields.Char(string='User Name', size=50, required=True)
+    password = fields.Char(string='Password', size=64, required=True)
+    obj_ids = fields.One2many('base.synchro.obj', 'server_id', string='Models', ondelete='cascade')
+
+class base_synchro_obj(models.Model):
     '''Class to store the operations done by wizard'''
     _name = "base.synchro.obj"
     _description = "Register Class"
-    _columns = {
-        'name':fields.char('Name', size=64, select=1, required=1),
-        'domain':fields.char('Domain', size=64, select=1, required=1),
-        'server_id':fields.many2one('base.synchro.server', 'Server', ondelete='cascade', select=1, required=1),
-        'model_id': fields.many2one('ir.model', 'Object to synchronize', required=True),
-        'action':fields.selection([('d', 'Download'), ('u', 'Upload'), ('b', 'Both')], 'Synchronisation direction', required=True),
-        'sequence': fields.integer('Sequence'),
-        'active': fields.boolean('Active'),
-        'synchronize_date':fields.datetime('Latest Synchronization', readonly=True),
-        'line_id':fields.one2many('base.synchro.obj.line', 'obj_id', 'Ids Affected', ondelete='cascade'),
-        'avoid_ids':fields.one2many('base.synchro.obj.avoid', 'obj_id', 'Fields Not Sync.'),
-    }
-    _defaults = {
-        'active': lambda *args: True,
-        'action': lambda *args: 'd',
-        'domain': lambda *args: '[]'
-    }
     _order = 'sequence'
+
+    name = fields.Char(string='Name', size=64, select=1, required=1)
+    domain = fields.Char(string='Domain', size=64, select=1, required=1,default='[]')
+    server_id = fields.Many2one('base.synchro.server', string='Server', ondelete='cascade', select=1, required=1)
+    model_id =  fields.Many2one('ir.model', string='Object to synchronize', required=True)
+    action = fields.Selection([('d', 'Download'), ('u', 'Upload'), ('b', 'Both')], string='Synchronisation direction', required=True, default='d')
+    sequence =  fields.Integer(string='Sequence')
+    active =  fields.Boolean(string='Active', default=True)
+    synchronize_date = fields.Datetime(string='Latest Synchronization', readonly=True)
+    line_id = fields.One2many('base.synchro.obj.line', 'obj_id', string='Ids Affected', ondelete='cascade')
+    avoid_ids = fields.One2many('base.synchro.obj.avoid', 'obj_id', string='Fields Not Sync.')
+
     #
     # Return a list of changes: [ (date, id) ]
     #
 
-    def get_ids(self, cr, uid, object, dt, domain=[], context=None):
-        return self._get_ids(cr, uid, object, dt, domain, context=context)
+    @api.model
+    def get_ids(self, object, dt, domain=[], action=None):
+        if action is None:
+            action = {}
+        return self._get_ids(object, dt, domain, action=action)
 
-    def _get_ids(self, cr, uid, object, dt, domain=[], context=None):
-        POOL = self.pool.get(object)
+    @api.model
+    def _get_ids(self, object, dt, domain=[], action=None):
+        if action is None:
+            action = {}
+        POOL = self.env[object]
         result = []
         if dt:
             domain2 = domain + [('write_date', '>=', dt)]
             domain3 = domain + [('create_date', '>=', dt)]
         else:
             domain2 = domain3 = domain
-        ids = POOL.search(cr, uid, domain2, context=context)
-        ids += POOL.search(cr, uid, domain3, context=context)
-        for r in POOL.read(cr, uid, ids, ['create_date','write_date'], context=context):
-            result.append((r['write_date'] or r['create_date'], r['id'], context.get('action', 'd')))
+        ids = POOL.search(domain2)
+        ids += POOL.search(domain3)
+        for r in ids.read(['create_date','write_date']):
+            result.append((r['write_date'] or r['create_date'], r['id'], action.get('action', 'd')))
         return result
 
-class base_synchro_obj_avoid(osv.Model):
+class base_synchro_obj_avoid(models.Model):
     _name = "base.synchro.obj.avoid"
     _description = "Fields to not synchronize"
-    _columns = {
-        'name':fields.char('Field Name', size=64, select=1, required=1),
-        'obj_id':fields.many2one('base.synchro.obj', 'Object', required=1, ondelete='cascade'),
-    }
 
-class base_synchro_obj_line(osv.Model):
+    name = fields.Char(string='Field Name', size=64, select=1, required=1)
+    obj_id = fields.Many2one('base.synchro.obj', 'Object', required=1, ondelete='cascade')
+
+class base_synchro_obj_line(models.Model):
     '''Class to store the operations done by wizard'''
     _name = "base.synchro.obj.line"
     _description = "Synchronized instances"
-    _columns = {
-        'name': fields.datetime('Date', required=True),
-        'obj_id': fields.many2one('base.synchro.obj', 'Object', ondelete='cascade', select=True),
-        'local_id': fields.integer('Local Id', readonly=True),
-        'remote_id':fields.integer('Remote Id', readonly=True),
-    }
-    _defaults = {
-        'name': lambda *args: time.strftime('%Y-%m-%d %H:%M:%S')
-    }
+
+    name = fields.Datetime('Date', required=True, default=lambda *args: time.strftime('%Y-%m-%d %H:%M:%S'))
+    obj_id = fields.Many2one('base.synchro.obj', 'Object', ondelete='cascade', select=True)
+    local_id = fields.Integer('Local Id', readonly=True)
+    remote_id = fields.Integer('Remote Id', readonly=True)
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
