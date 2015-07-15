9f4ec483404b1cbf90ab9bc45ccabeb4ab8aeca5
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-07-15 17:11:55 +0530

    [ADD]Added web_widget_multi_image module.

---
+++ web_widget_multi_image/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#    Copyright (C) 2011-2015 Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Web Widget Multi Image",
+    "version" : "1.0",
+    "author" : "Serpent Consulting Services Pvt. Ltd.",
+    "category": 'Image',
+    'complexity': "easy",
+    'depends': ['product'],
+    "description": """
+    """,
+    'data': ['product_view.xml'],
+    'js': [
+        'static/lib/lightbox/js/jquery.lightbox.js',
+        'static/src/js/multi_image.js',
+    ],
+    'css': [
+        'static/lib/lightbox/css/lightbox.css',
+        'static/src/css/hoverbox.css',
+        ],
+    'website': 'http://www.serpentcs.com',
+    'qweb': ['static/src/xml/image_multi.xml'],
+    'installable': True,
+    'auto_install': False,
+}
