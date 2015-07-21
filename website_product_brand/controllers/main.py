2ddb0d8912fa99ae5874cf3ce720d92b50d9b7e7
punit <punit@punit.(none)>
2015-07-21 11:04:14 +0530

    Solved build-1 issue and also improve the code in scs contribuation.

--- website_product_brand/controllers/main.py
+++ website_product_brand/controllers/main.py
@@ -110,12 +110,9 @@ class website_sale(openerp.addons.website_sale.controllers.main.website_sale):
         from_currency = pool.get('product.price.type')._get_field_currency
         (cr, uid, 'list_price', context)
         to_currency = pricelist.currency_id
-        compute_currency = lambda price: pool['res.currency']._compute(
-                                                                       cr,uid,
-                                                                       from_currency,
-                                                                       to_currency,
-                                                                       price,
-                                                                       context=context)
+        compute_currency = lambda price: pool
+        ['res.currency']._compute(cr, uid, from_currency,
+                                  to_currency, price, context=context)
         values.update({'search': search,
                        'category': category,
                        'attrib_values': attrib_values,
