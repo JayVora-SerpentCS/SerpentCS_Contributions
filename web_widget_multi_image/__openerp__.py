d203cb42cdc142ee2c83984dbab1f2e605aa00b0
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-08-06 19:00:12 +0530

    [ADD]Added company logo in web multi image.

--- web_widget_multi_image/__openerp__.py
+++ web_widget_multi_image/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     "name" : "Web Widget Multi Image V8",
-    "version" : "1.0",
+    "version" : "1.2",
     "author" : "Serpent Consulting Services Pvt. Ltd.",
     "category": 'Image',
     'complexity': "easy",
