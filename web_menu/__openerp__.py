32daa84c91d479e26a2c5b3ee1456659690545d0
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-08-12 19:33:38 +0530

    [ADD]Added web menu module.

---
+++ web_menu/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011-Till Today Serpent Consulting Services PVT. LTD.
+#    (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Web Menu',
+    'version': '1.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'description': '',
+    'category': 'Web',
+    'depends': ['web',],
+    'data': [
+        'views/templates.xml',
+    ],
+    'qweb' : [
+        "static/src/xml/*.xml",
+    ],
+    'application': True,
+    'auto_install': False,
+}
