4b26423b65d8786b9ea07a1f3945dd93649c194e
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2015-08-19 19:37:21 +0530

    Added project_team module

---
+++ project_team/sale_team.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012-Today Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp import models, fields,_,api
+
+class crm_case_section(models.Model):
+    
+    _inherit = 'crm.case.section'
+    
+    type = fields.Selection([('sale','Sale'),('project','Project')],string="Type",default="sale")
+    parent_id = fields.Many2one('crm.case.section', string='Parent Team')
+    
+class project_project(models.Model):
+    
+    _inherit = 'project.project'
+    
+    team_ids = fields.Many2one('crm.case.section',string="Project Team",domain=[('type','=','project')])
+    
+    @api.onchange('team_ids')
+    def get_team_member(self):
+        lst = []
+        for rec in self.team_ids.member_ids:
+            lst.append(rec.id)
+        self.members = [(4,lst)]
+        
