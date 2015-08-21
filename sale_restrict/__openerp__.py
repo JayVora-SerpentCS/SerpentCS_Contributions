cbbff3ed254df0d6b2c86c9585bfc606b270d292
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2015-08-21 19:25:57 +0530

    Added Sale_restrict module

---
+++ sale_restrict/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "SO - Product Price Check",
+
+    'summary': """""",
+    'description': """
+        This modules restricts a user from confirming a Sale Order/Quotation if it contains products having sale price zero.
+    """,
+
+    'author': "Serpent Consulting Services Pvt. Ltd.",
+    'website': "http://www.serpentcs.com",
+
+    'category': 'Sales Management',
+    'version': '1.1',
+
+    'depends': ['sale'],
+
+    # always loaded
+    'data': [
+             
+             ],
+    # only loaded in demonstration mode
+    'demo': [
+             
+    ],
+}
