7870a782b8c648321452b864275d570472bab450
loikki <loic_hausammann@hotmail.com>
2015-10-20 15:31:37 +0200

    remove correctly the menu in more

--- label/label_print.py
+++ label/label_print.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models,fields, osv, _
+from openerp import models, fields, api, _
+
 
 class label_print(models.Model):
     _name = "label.print"
@@ -8,11 +9,13 @@ class label_print(models.Model):
     name = fields.Char("Name", size=64, required=True, select=1)
     model_id = fields.Many2one('ir.model', 'Model', required=True, select=1)
     field_ids = fields.One2many("label.print.field", 'report_id', 'Fields')
-    ref_ir_act_report = fields.Many2one('ir.actions.act_window', 'Sidebar action', readonly=True,
-                                        help="Sidebar action to make this template available on records "
-                                        "of the related document model")
-    ref_ir_value = fields.Many2one('ir.values', 'Sidebar button', readonly=True,
-                                   help="Sidebar button to open the sidebar action")
+    ref_ir_act_report = fields.Many2one(
+        'ir.actions.act_window', 'Sidebar action', readonly=True,
+        help="Sidebar action to make this template available on records "
+        "of the related document model")
+    ref_ir_value = fields.Many2one(
+        'ir.values', 'Sidebar button', readonly=True,
+        help="Sidebar button to open the sidebar action")
     model_list = fields.Char('Model List', size=256)
 
     def onchange_model(self, cr, uid, ids, model_id):
@@ -24,7 +27,8 @@ class label_print(models.Model):
             active_model_obj = self.pool.get(model_data.model)
             if active_model_obj._inherits:
                 for key, val in active_model_obj._inherits.items():
-                    model_ids = model_obj.search(cr, uid, [('model', '=', key)])
+                    model_ids = model_obj.search(
+                        cr, uid, [('model', '=', key)])
                     if model_ids:
                         model_list += "," + str(model_ids[0]) + ""
             model_list += "]"
@@ -50,29 +54,24 @@ class label_print(models.Model):
                  'name': button_name,
                  'model': src_obj,
                  'key2': 'client_action_multi',
-                 'value': "ir.actions.act_window," + str(vals['ref_ir_act_report']),
+                 'value': "ir.actions.act_window," + str(
+                     vals['ref_ir_act_report']),
                  'object': True,
              }, context)
         self.write(cr, uid, ids, {
-                    'ref_ir_act_report': vals.get('ref_ir_act_report',False),
-                    'ref_ir_value': vals.get('ref_ir_value',False),
+                    'ref_ir_act_report': vals.get('ref_ir_act_report', False),
+                    'ref_ir_value': vals.get('ref_ir_value', False),
                 }, context)
         return True
 
-    def unlink_action(self, cr, uid, ids, context=None):
-        ir_values_obj = self.pool.get('ir.values')
-        act_window_obj = self.pool.get('ir.actions.act_window')
-        for template in self.browse(cr, uid, ids, context=context):
-            try:
-                if template.ref_ir_act_report:
-                    act_window_obj.unlink(cr, uid, template.ref_ir_act_report.id, context)
-                if template.ref_ir_value:
-                    ir_values_obj.unlink(cr, uid, template.ref_ir_value.id, context)
-            except Exception, e:
-                raise osv.except_osv(_("Warning"), _("Deletion of the action record failed. %s" % (e)))
-        return True
+    @api.one
+    def unlink(self):
+        self.ref_ir_act_report.unlink()
+        self.ref_ir_value.unlink()
+        
 
 label_print()
+
 
 class label_print_field(models.Model):
     _name = "label.print.field"
@@ -81,13 +80,16 @@ class label_print_field(models.Model):
     sequence = fields.Integer("Sequence", required=True)
     field_id = fields.Many2one('ir.model.fields', 'Fields', required=False)
     report_id = fields.Many2one('label.print', 'Report')
-    type_ = fields.Selection([('normal','Normal'), ('barcode', 'Barcode'), ('image', 'Image')], 'Type', required=True, default='normal')
+    type_ = fields.Selection([
+        ('normal', 'Normal'), ('barcode', 'Barcode'), ('image', 'Image')],
+                             'Type', required=True, default='normal')
     python_expression = fields.Boolean('Python Expression')
     python_field = fields.Char('Fields', size=32)
     fontsize = fields.Float("Font Size", default=8.0)
-    position = fields.Selection([('left','Left'),('right','Right'),('top','Top'),('bottom','Bottom')],'Position')
+    position = fields.Selection([('left', 'Left'), ('right', 'Right'),
+                                 ('top', 'Top'), ('bottom', 'Bottom')],
+                                'Position')
     nolabel = fields.Boolean('No Label')
     newline = fields.Boolean('New Line', default=True)
 
 label_print_field()
-
