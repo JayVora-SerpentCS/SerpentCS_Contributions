3cedd28b740733811ab0d97dd9785396baf21bf9
MeetSCS <m.dholakia.serpentcs@gmail.com>
2015-12-01 14:37:35 +0530

    Migrated digital sign and one2many kanban

---
+++ web_digital_sign/users.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from openerp import models, fields, api
+
+class Users(models.Model):
+    _name = 'res.users'
+    _inherit = 'res.users'
+
+    signature= fields.Binary(string='Signature')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
