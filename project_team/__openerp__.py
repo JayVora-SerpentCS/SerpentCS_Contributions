0acf746da97dd820af28b9a3cdfd36104499c5e8
Hemangini Patel <h.patel.serpentcs@gmail.com>
2015-12-17 16:16:27 +0530

    [ADD]project_team: Added project team module in v9.

--- project_team/__openerp__.py
+++ project_team/__openerp__.py
@@ -1,34 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-Today Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-##############################################################################
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name':'Project Team',
     'author':'Serpent Consulting Services Pvt. Ltd.',
+    'summary':'Adds Project Team Members.',
     'category': 'Project Management.',
     'website':'http://www.serpentcs.com',
-    'version':'1.0',
+    'version':'9.0.1.0.0',
     'sequence': 1,
     'depends':['project','crm'],
     'data':[
-            'view/project_team_view.xml'
+            'views/project_team_view.xml'
     ],
     'installable':True,
     'auto_install':False
