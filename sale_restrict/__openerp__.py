a9814deaef3bce49b06df902cd9b71527c975d78
Hemangini Patel <h.patel.serpentcs@gmail.com>
2015-12-22 12:24:21 +0530

    [ADD]sale_restrict: added sale restrict module.

--- sale_restrict/__openerp__.py
+++ sale_restrict/__openerp__.py
@@ -1,26 +1,17 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': "SO - Product Price Check",
-
-    'summary': """""",
+    'version': '9.0.1.1.0',
+    'category': 'Sales Management',
     'description': """
-        This module restricts a user from confirming a Sale Order/Quotation if it contains products having sale price zero.
+        This module restricts a user from confirming a Sale Order/Quotation 
+        if it contains products having sale price zero.
     """,
-
     'author': "Serpent Consulting Services Pvt. Ltd.",
     'website': "http://www.serpentcs.com",
-
-    'category': 'Sales Management',
-    'version': '1.1',
-
     'depends': ['sale'],
-
-    # always loaded
-    'data': [
-             
-             ],
-    # only loaded in demonstration mode
-    'demo': [
-             
-    ],
+    'installable': True,
+    'auto_install':False,
 }
