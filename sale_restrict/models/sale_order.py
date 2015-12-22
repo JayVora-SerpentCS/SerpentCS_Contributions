face447ca1dda4527d214930ffa3476179e3a394
Hemangini Patel <h.patel.serpentcs@gmail.com>
2015-12-22 12:24:21 +0530

    [ADD]sale_restrict: added sale restrict module.

--- sale_restrict/models/sale_order.py
+++ sale_restrict/models/sale_order.py
@@ -1,15 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 from openerp import models,fields,api,_
 from openerp.exceptions import Warning
 
+
 class sale_order(models.Model):
     _inherit = 'sale.order'
-    
+
     @api.multi
-    def action_button_confirm(self):
+    def action_confirm(self):
+
         zero_price = [x.product_id.name for x in self.order_line if not x.price_unit]
+
         if zero_price:
             message= _("Please specify unit price for the following products:") + '\n'
             message += '\n'.join(map(str,zero_price))
             raise Warning(message.rstrip())
-        return super(sale_order,self).action_button_confirm()
-    
+        return super(sale_order,self).action_confirm()
