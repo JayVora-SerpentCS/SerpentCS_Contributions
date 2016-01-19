9f27dca1d00bc08c153ecbce49a58f2ea1c02f68
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2016-01-19 19:24:52 +0530

    [ADD] sale_report_hide_price added to hide price and discount in sale report

---
+++ sale_report_hide_price/models/sale.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2012-Today Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class sale_order(models.Model):
+
+    _inherit = 'sale.order'
+
+    show_price = fields.Boolean('Show Price', help="""If ticked, you can see
+                    the price & discount in report of sale order/quotation""")
+    show_discount = fields.Boolean('Show Discount', help="""If ticked, you can 
+		    see the discount in report of sale order/quotation""")
