ae94f6e35ac74b9aa3e399877a911cf092587ed1
MeetSCS <m.dholakia.serpentcs@gmail.com>
2016-02-16 18:46:06 +0530

    [ADD]Added funnel chart for lead and opportunity.

---
+++ web_lead_funnel_chart/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011-Today Serpent Consulting Services Pvt. Ltd.
+#                                     (<http://www.serpentcs.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+{
+    'name': 'Web Lead Funnel Chart',
+    'category': 'Web',
+    'author': "Serpent Consulting Services Pvt. Ltd.",
+    'summary': 'Funnel Chart for Leads & Opportunities',
+    'website': 'http://www.serpentcs.com',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'crm'
+    ],
+    'data': [
+        "views/templates.xml",
+        "views/web_lead_funnel_chart_view.xml"
+    ],
+    'qweb': [
+        'static/src/xml/*.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
