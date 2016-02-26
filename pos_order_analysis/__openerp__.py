f1d000f1093c7be64ad5b492dde451bee9470ae8
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2016-02-26 16:40:24 +0530

    [ADD] Added the pos_order_analysis module

---
+++ pos_order_analysis/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#    Copyright (C) 2011-today Serpent Consulting Services Pvt. Ltd. 
+#                                               (<http://www.serpentcs.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "POS Order Analysis with Highchart",
+    "version": "1.0.0",
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'website': 'http://www.serpentcs.com',
+    "category": "Tools",
+    "sequence": 1,
+    "depends": ['point_of_sale'],
+    "data":[
+            "views/templates.xml",
+            "pos_high_chart_view.xml"
+         ],
+    "qweb": ["static/src/xml/pos_high_chart.xml"],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+}
