ce51728b30e3df03bf098bae4376de82eb5715ca
MeetSCS <m.dholakia.serpentcs@gmail.com>
2016-05-05 17:57:54 +0530

    [FIX]fixed the issue on group by module closing #46

--- web_groupby_expand/__openerp__.py
+++ web_groupby_expand/__openerp__.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-Till Today Serpent Consulting Services PVT. LTD.
-#    (<http://www.serpentcs.com>)
+#                                          (<http://www.serpentcs.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -22,18 +22,21 @@
 
 {
     "name": "Web GroupBy Expand",
-    "version": "1.2",
-    "author": "Serpent Consulting Services Pvt. Ltd.",
-    "website": "http://www.serpentcs.com",
-    'category' : 'Web',
-    "description": "Adds Expand button to expand all groups on a list view.",
-    "depends": ["web"],
-    'demo' : [],
-    'data': [
-        'views/templates.xml',
-    ],
-    "qweb": [ "static/src/xml/web_groups_expand.xml" ],
+    "category": "Web",
+    "version" : '7.0.1.0.0',
+    "author" : 'Serpent Consulting Services Pvt. Ltd.',
+    "description":
+        """
+A group by list can be expanded and collapased with buttons
+===============================================================
+You'll see two buttons appear on top right corner of the list when you perform a group by with which you can expand and collapse grouped records by level.
+        """,
+    "depends" : ["web"],
+    "js": ["static/src/js/web_group_expand.js"],
+    "css" : ["static/src/lib/font-awesome-4.5.0/css/font-awesome.css",
+             "static/src/css/expand_buttons.css"],
+    "qweb" : ["static/src/xml/expand_buttons.xml"],
+    "installable": True,
     "auto_install": False,
 }
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- web_groupby_expand/static/src/js/web_group_expand.js
+++ web_groupby_expand/static/src/js/web_group_expand.js
@@ -1,8 +1,7 @@
-openerp.web_groupby_expand = function (openerp) {
-    var _t = openerp.web._t,
-        _lt = openerp.web._lt;
+openerp.web_groupby_expand = function(openerp) {
     var QWeb = openerp.web.qweb;
-
+    var _t = openerp.web._t,
+    _lt = openerp.web._lt;
     openerp.web.ListView.Groups.include({
         init:function (view, options) {
             this._super(view, options);
@@ -18,8 +17,6 @@ openerp.web_groupby_expand = function (openerp) {
                     .filter(function (column) { return column.tag === 'field' })
                     .pluck('name').value(),
                 function (groups) {
-                    self.view.$pager.find('.oe_pager_group').hide();
-                    self.view.$pager.find('.oe_list_pager_state').text(self.view._limit ? self.view._limit : '∞');
                     $el[0].appendChild(
                         self.render_groups(groups));
                     if (post_render) { post_render(); }
@@ -27,17 +24,17 @@ openerp.web_groupby_expand = function (openerp) {
                         self.render_auto_groups(self.groups_auto);
                     }
                 }, function (dataset) {
-                    self.render_dataset(dataset).done(function (list) {
+                    self.render_dataset(dataset).then(function (list) {
                         self.children[null] = list;
                         self.elements =
                             [list.$current.replaceAll($el)[0]];
                         self.setup_resequence_rows(list, dataset);
+                    }).always(function() {
                         if (post_render) { post_render(); }
                     });
                 });
             return $el;
         },
-
         render_groups: function (datagroups) {
             var self = this;
             var placeholder = this.make_fragment();
@@ -110,6 +107,7 @@ openerp.web_groupby_expand = function (openerp) {
                     // escaping if format failed), can inject straight into HTML
                     $group_column.html(_.str.sprintf(_t("%s (%d)"),
                         group_label, group.length));
+
                     if (group.length && group.openable) {
                         // Make openable if not terminal group & group_by_no_leaf
                         $group_column.prepend('<span class="ui-icon ui-icon-triangle-1-e" style="float: left;">');
@@ -146,7 +144,6 @@ openerp.web_groupby_expand = function (openerp) {
             });
             return placeholder;
         },
-
         render_auto_groups: function (groups_auto) {
             var self = this;
             if (!groups_auto) {
@@ -174,17 +171,16 @@ openerp.web_groupby_expand = function (openerp) {
                 }
             });
         },
-    });
-    
+    })
     openerp.web.ListView.include({
+
         set_default_options: function (options) {
             this._super(options);
             _.defaults(this.options, {
                 expand : false,
-//                GroupsType: openerp.web.ListView.Groups,
-//                ListType: openerp.web.ListView.List
             });
         },
+
         load_list: function(data) {
             var self = this;
             if (!this.$buttons) {
@@ -208,19 +204,52 @@ openerp.web_groupby_expand = function (openerp) {
                 $('.oe-list-expand').show();
             }
         },
+
         do_search: function (domain, context, group_by) {
             this._super(domain, context, group_by);
             this.options.expand = false;
             this.groups.groups_auto = []
         },
+
     });
-    
-    openerp.web.ViewManagerAction.include({
-        switch_mode: function (view_type, no_store, options) {
-            this._super.apply(this, arguments);
-            if(view_type != 'list'){
-                $('.oe-list-expand').hide();
-            }
-        }, 
-    });
-};
+//    openerp.web.ViewManager.include({
+//        switch_mode: function(view_type, no_store, view_options) {
+//            if (view_type != 'list' && view_type != 'tree' ) {
+//                this.$el.find("ul#oe_group_by").remove();
+//            }
+//            if(view_type == 'tree'){
+//                this.load_expand_buttons();
+//                this.$ExpandButtons.find("a#oe_group_by_reset").click(function(){
+//                    $('.oe_open .treeview-tr.oe-treeview-first').filter(function(){return ($(this).parents('tr').attr('data-level') == 1)}).click()
+//                });
+//                this.$ExpandButtons.find("a#oe_group_by_expand").click(function(){
+//                    $('.treeview-tr.oe-treeview-first').filter(function(){return (!$(this).parents().is('.oe_open')) & ($(this).parents().css( "display" ) != 'none')}).click();
+//                });
+//            }
+//            return this._super.apply(this, arguments);
+//        },
+//        expand: function(domains, contexts, groupbys) {
+//            this.$el.find("ul#oe_group_by").remove();
+//            if(groupbys.length && this.active_view == 'list') {
+//            	this.load_expand_buttons();	
+//                this.$el.find("a#oe_group_by_reset").click(function(){
+//                    $('span.ui-icon-triangle-1-s').click()
+//                });
+//                this.$el.find("a#oe_group_by_expand").click(function(){
+//                    $('span.ui-icon-triangle-1-e').click()
+//                });
+//            }
+//        },
+//        load_expand_buttons:function() {
+//            var self = this;
+//            this.$ExpandButtons = $(QWeb.render("GroupExpand.Buttons", {'widget':self}));
+//            this.$el.find("ul.oe_view_manager_switch.oe_button_group.oe_right").before(this.$ExpandButtons);
+//        },
+//        setup_search_view: function(view_id, search_defaults) {
+//            self = this;
+//            res = this._super.apply(this, arguments);
+//            this.searchview.on('search_data', self, this.expand);
+//            return res
+//        },
+//    })
+}
