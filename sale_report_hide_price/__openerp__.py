ae99279d901371dee7c47762b16a6565ae564228
Serpent Consulting Services Pvt. Ltd <jay.vora@serpentcs.com>
2016-08-12 13:39:36 +0530

    sale_report_hide_price module

---
+++ sale_report_hide_price/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd. <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Hide Price and Discount in Quotation Report',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'category': 'Sales Management',
+    'website': 'http://www.serpentcs.com',
+    'version': '9.0.1.0.1',
+    'license': 'AGPL-3',
+    'summary': 'Hide price/discount in Sale Order Report',
+    'description': '''This module is to hide price/discount in Sale Order
+Report.
+It would give you 2 additional fields on Sales Order / Quotation:
+1. Show Price
+2. Show Discount
+Based on the choices on the fields, the relevant fields would be shown on
+the report.''',
+    'depends': ['sale'],
+    'data': ['views/sale_view.xml',
+             'views/report_saleorder.xml'
+             ],
+    'installable': True,
+    'auto_install': False
+}
---
+++ sale_report_hide_price/models/sale.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Serpent Consulting Services Pvt. Ltd. <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import fields, models
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    show_price = fields.Boolean('Show Price', help="If checked, you can see"
+                                " the price & discount in report of Sales"
+                                " Order / Quotation.")
+    show_discount = fields.Boolean('Show Discount', help="If checked, you"
+                                    " can see the discount in report of"
+                                    " Sales Order / quotation.")
