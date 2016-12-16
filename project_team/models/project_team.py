79a09ee775b47c8c258f2b75ebdc0ddfa0ec8997
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-16 13:37:53 +0530

    [REF]

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -5,32 +5,30 @@ from openerp import models, fields, api, _
 
 
 class CrmTeamInherit(models.Model):
-
     _inherit = 'crm.team'
 
     type_team = fields.Selection([('sale', 'Sale'), ('project', 'Project')],
                                  string="Type", default="sale")
 
     team_members = fields.Many2many('res.users', 'project_team_user_rel',
-                                    'team_id','uid', 'Project Members',
+                                    'team_id', 'uid', 'Project Members',
                                     help="""Project's members are users who
                                      can have an access to the tasks related
                                      to this project.""")
 
 
 class ProjectProject(models.Model):
-
     _inherit = 'project.project'
 
     members = fields.Many2many('res.users', 'project_user_rel', 'project_id',
                                'uid', 'Project Members', help="""Project's
                                members are users who can have an access to
                                the tasks related to this project.""",
-                               states={'close':[('readonly',True)],
-                                       'cancelled':[('readonly',True)]})
+                               states={'close': [('readonly', True)],
+                                       'cancelled': [('readonly', True)]})
     team_id = fields.Many2one('crm.team', string="Project Team",
                               domain=[('type_team', '=', 'project')])
 
     @api.onchange('team_id')
     def get_team_members(self):
-        self.members = [(6,0,[rec.id for rec in self.team_id.team_members])]
+        self.members = [(6, 0, [rec.id for rec in self.team_id.team_members])]
