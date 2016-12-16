1fa8de01bd54b031eb9c73a11acf5b42ec2cf31d
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-16 13:37:53 +0530

    [REF]

--- project_team/__openerp__.py
+++ project_team/__openerp__.py
@@ -2,17 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name':'Project Team',
-    'author':'Serpent Consulting Services Pvt. Ltd.',
-    'summary':'Adds Project Team Members.',
+    'name': 'Project Team',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'summary': 'Adds Project Team Members.',
     'category': 'Project Management.',
-    'website':'http://www.serpentcs.com',
-    'version':'9.0.1.0.2',
+    'website': 'http://www.serpentcs.com',
+    'version': '9.0.1.0.2',
     'sequence': 1,
-    'depends':['project','crm'],
-    'data':[
-            'views/project_team_view.xml'
+    'depends': ['project', 'crm'],
+    'data': [
+        'views/project_team_view.xml'
     ],
-    'installable':True,
-    'auto_install':False
+    'installable': True,
+    'auto_install': False
 }
