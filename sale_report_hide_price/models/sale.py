0b68c7638392cc8c590195973810fdfc01bce15a
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-16 13:37:53 +0530

    [REF]

--- sale_report_hide_price/models/sale.py
+++ sale_report_hide_price/models/sale.py
@@ -9,8 +9,8 @@ class sale_order(models.Model):
     _inherit = 'sale.order'
 
     show_price = fields.Boolean('Show Price', help="If checked, you can see"
-                                " the price in report of Sales"
-                                " Order / Quotation.")
+                                                   " the price in report of Sales"
+                                                   " Order / Quotation.")
     show_discount = fields.Boolean('Show Discount', help="If checked, you"
-                                    " can see the discount in report of"
-                                    " Sales Order / quotation.")
+                                                         " can see the discount in report of"
+                                                         " Sales Order / quotation.")
