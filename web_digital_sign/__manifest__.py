fbd821ff3d96ec88ac066d31b1f2ef1bebe518d1
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-28 22:04:45 +0530

    PEP8 fixes

--- web_digital_sign/__manifest__.py
+++ web_digital_sign/__manifest__.py
@@ -8,7 +8,7 @@
     'complexity': "easy",
     'depends': ['web'],
     "description": """
-        This module provides the functionality to store digital signature 
+        This module provides the functionality to store digital signature
 	image for a record.
         The example can be seen into the User's form view where we have added
 	a test field under signature.
