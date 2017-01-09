0a41710b83d40771c475aad8decb8a55e5497c49
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-01-09 13:36:03 +0530

    [MIG] added sale_cancel_reason

--- sale_cancel_reason/model/__init__.py
+++ sale_cancel_reason/model/__init__.py
@@ -4,5 +4,4 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd. (http://www.serpentcs.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import wizard
-from . import model
+from . import sale
