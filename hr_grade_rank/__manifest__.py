8cd41246858b5b79996af4c51232e80c62a6a7f2
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-01-25 17:43:23 +0530

    [ADD] Added hr_grade_rank module to allow manage your HR with box system

---
+++ hr_grade_rank/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2017 Serpent Consulting Services Pvt. Ltd.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'HR-Grade Rank',
+    'version': '1.0',
+    'description': """
+HR-Grade Rank
+=============
+This module is used to add grade of employees.
+        """,
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'category': 'Human Resources',
+    'website': 'https://www.serpentcs.com',
+    'depends': ['hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hr_grade_rank_view.xml',
+    ],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
