1de5794f0c8db2d9936cb4aa5d2fc7503ea21adf
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-03-23 11:12:28 +0530

    [FIX] fixed the issue with sale_layout conflict closes #32

--- sales_report_product_image/__openerp__.py
+++ sales_report_product_image/__openerp__.py
@@ -28,7 +28,7 @@
     'website': 'http://www.serpentcs.com',
     'version': '8.0.1.0.0',
     'sequence': 1,
-    'depends': ['sale', 'web_tree_image'],
+    'depends': ['sale_layout', 'web_tree_image'],
     'data': [
         'views/sale_product_view.xml',
         'views/report_saleorder.xml',
