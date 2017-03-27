10cbebdf5dfd46a6f45b33d6e81d6ef925a0a74c
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2017-03-27 12:44:53 +0530

    [FIX] Replaced the EAN13 barcode type with Code128 to solve the issue in barcode label.

--- label/models/label_config.py
+++ label/models/label_config.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 
-from openerp import models,fields,api
+from openerp import models, fields, api
 
 
 class label_main(models.Model):
@@ -39,9 +39,9 @@ class label_config(models.Model):
     name = fields.Char("Name", size=64, required=True, select=1)
     height = fields.Float("Height (in mm)", required=True)
     width = fields.Float("Width (in mm)", required=True)
-    top_margin = fields.Float("Top Margin (in mm)",default=0.0)
-    bottom_margin = fields.Float("Bottom Margin  (in mm)",default=0.0)
-    left_margin = fields.Float("Left Margin (in mm)",default=0.0)
-    right_margin = fields.Float("Right Margin (in mm)",default=0.0)
-    cell_spacing = fields.Float("Cell Spacing",default=1.0)
+    top_margin = fields.Float("Top Margin (in mm)", default=0.0)
+    bottom_margin = fields.Float("Bottom Margin  (in mm)", default=0.0)
+    left_margin = fields.Float("Left Margin (in mm)", default=0.0)
+    right_margin = fields.Float("Right Margin (in mm)", default=0.0)
+    cell_spacing = fields.Float("Cell Spacing", default=1.0)
     label_main_id = fields.Many2one('label.brand', 'Label')
