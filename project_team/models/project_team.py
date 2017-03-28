7451b8bd5a77caee3e8434ea8f6246713d587b06
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2017-03-29 01:00:55 +0530

    [MIG] project_team migrated to v10

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -18,6 +18,7 @@ class CrmTeamInherit(models.Model):
 
 
 class ProjectProject(models.Model):
+
     _inherit = 'project.project'
 
     members = fields.Many2many('res.users', 'project_user_rel', 'project_id',
@@ -30,4 +31,11 @@ class ProjectProject(models.Model):
 
     @api.onchange('team_id')
     def get_team_members(self):
-        self.members = [(6, 0, [rec.id for rec in self.team_id.team_members])]
+        self.members = [(4, [rec.id for rec in self.team_id.team_members])]
+
+    # @api.model
+    # def create(self, vals):
+    #     res = super(ProjectProject, self).create(vals)
+    #     if res.team_id and not res.members:
+    #         res.write({'members': [(4, res.team_id.team_members.ids)]})
+    #     return res
