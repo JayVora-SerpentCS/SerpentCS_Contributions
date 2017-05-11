cdc9b18886f795aee8c6f0080be67c5ebcf33f0d
Hemangini Patel <h.patel.serpentcs@gmail.com>
2017-05-11 11:24:27 +0530

    [PATCH 06/28] [FIX]Travis Fixed.
    [IMP]Improved code in js.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.

--- partner_credit_limit/models/__init__.py
+++ partner_credit_limit/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # See LICENSE file for full copyright and licensing details.
 
-import partner
-import sale
+from . import partner
+from . import sale
