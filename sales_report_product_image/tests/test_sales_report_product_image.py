7e55cc228813db189f45a8240ba798e043f7fd9d
Hemangini Patel <h.patel.serpentcs@gmail.com>
2017-05-11 11:24:27 +0530

    [FIX]Travis Fixed.
    [IMP]Improved code in js.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.

--- sales_report_product_image/tests/test_sales_report_product_image.py
+++ sales_report_product_image/tests/test_sales_report_product_image.py
@@ -2,11 +2,11 @@
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # See LICENSE file for full copyright and licensing details.
 
-from odoo.exceptions import UserError, AccessError
 from odoo.tests import common
 
 
 class SaleReportProductImageTestCase(common.TransactionCase):
+
     def setup(self):
         super(SaleReportProductImageTestCase, self).setup()
 
@@ -15,12 +15,13 @@ class SaleReportProductImageTestCase(common.TransactionCase):
         self.product = self.env.ref('product.product_product_7')
         self.partner = self.env.ref('base.res_partner_2')
 
-        so = self.env['sale.order'].create({
-            'partner_id' : self.partner.id,
-            'partner_invoice_id' : self.partner.id,
-            'partner_shipping_id': self.partner.id,
-            'pricelist_id' : self.env.ref('product.list0').id,
-            'print_image' : 'True',
-            'image_sizes' : 'image_medium',
-            'image_small' : self.product.image_small,
-        })
+        self.env['sale.order'].create(
+            {
+                'partner_id': self.partner.id,
+                'partner_invoice_id': self.partner.id,
+                'partner_shipping_id': self.partner.id,
+                'pricelist_id': self.env.ref('product.list0').id,
+                'print_image': 'True',
+                'image_sizes': 'image_medium',
+                'image_small': self.product.image_small,
+            })
