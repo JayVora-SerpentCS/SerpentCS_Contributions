0b791b650146ac77e3d71a2aff13f0ad2d94a1d0
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2017-05-20 14:39:09 +0530

    [FIX] Fixed the bug of base_synchro in v8.

--- base_synchro/models/base_synchro_obj.py
+++ base_synchro/models/base_synchro_obj.py
@@ -23,6 +23,7 @@
 import time
 from openerp import models, fields, api
 
+
 class base_synchro_server(models.Model):
     '''Class to store the information regarding server'''
     _name = "base.synchro.server"
@@ -34,7 +35,9 @@ class base_synchro_server(models.Model):
     server_db = fields.Char('Server Database', required=True)
     login = fields.Char('User Name', required=True)
     password = fields.Char('Password', required=True)
-    obj_ids = fields.One2many('base.synchro.obj', 'server_id', string='Models', ondelete='cascade')
+    obj_ids = fields.One2many('base.synchro.obj', 'server_id',
+                              string='Models', ondelete='cascade')
+
 
 class base_synchro_obj(models.Model):
     '''Class to store the operations done by wizard'''
@@ -44,14 +47,21 @@ class base_synchro_obj(models.Model):
 
     name = fields.Char('Name', select=1, required=True)
     domain = fields.Char('Domain', select=1, required=True, default='[]')
-    server_id = fields.Many2one('base.synchro.server', 'Server', ondelete='cascade', select=1, required=True)
-    model_id =  fields.Many2one('ir.model', string='Object to synchronize', required=True)
-    action = fields.Selection([('d', 'Download'), ('u', 'Upload'), ('b', 'Both')], 'Synchronisation direction', required=True, default='d')
-    sequence =  fields.Integer('Sequence')
-    active =  fields.Boolean('Active', default=True)
+    server_id = fields.Many2one('base.synchro.server', 'Server',
+                                ondelete='cascade', select=1, required=True)
+    model_id = fields.Many2one('ir.model', string='Object to synchronize',
+                               required=True)
+    action = fields.Selection([('d', 'Download'), ('u', 'Upload'),
+                               ('b', 'Both')],
+                              'Synchronisation direction',
+                              required=True, default='d')
+    sequence = fields.Integer('Sequence')
+    active = fields.Boolean('Active', default=True)
     synchronize_date = fields.Datetime('Latest Synchronization', readonly=True)
-    line_id = fields.One2many('base.synchro.obj.line', 'obj_id', 'IDs Affected', ondelete='cascade')
-    avoid_ids = fields.One2many('base.synchro.obj.avoid', 'obj_id', 'Fields Not Sync.')
+    line_id = fields.One2many('base.synchro.obj.line', 'obj_id',
+                              'IDs Affected', ondelete='cascade')
+    avoid_ids = fields.One2many('base.synchro.obj.avoid', 'obj_id',
+                                'Fields Not Sync.')
 
     #
     # Return a list of changes: [ (date, id) ]
@@ -77,23 +87,30 @@ class base_synchro_obj(models.Model):
         obj_rec = POOL.search(domain2)
         obj_rec += POOL.search(domain3)
         for r in obj_rec.read(['create_date', 'write_date']):
-            result.append((r['write_date'] or r['create_date'], r['id'], action.get('action', 'd')))
+            result.append((r['write_date'] or r['create_date'], r['id'],
+                           action.get('action', 'd')))
         return result
+
 
 class base_synchro_obj_avoid(models.Model):
     _name = "base.synchro.obj.avoid"
     _description = "Fields to not synchronize"
 
     name = fields.Char('Field Name', select=1, required=True)
-    obj_id = fields.Many2one('base.synchro.obj', 'Object', required=True, ondelete='cascade')
+    obj_id = fields.Many2one('base.synchro.obj', 'Object', required=True,
+                             ondelete='cascade')
+
 
 class base_synchro_obj_line(models.Model):
     '''Class to store the operations done by wizard'''
     _name = "base.synchro.obj.line"
     _description = "Synchronized instances"
 
-    name = fields.Datetime('Date', required=True, default=lambda *args: time.strftime('%Y-%m-%d %H:%M:%S'))
-    obj_id = fields.Many2one('base.synchro.obj', 'Object', ondelete='cascade', select=1)
+    name = fields.Datetime(
+        'Date', required=True,
+        default=lambda *args: time.strftime('%Y-%m-%d %H:%M:%S'))
+    obj_id = fields.Many2one('base.synchro.obj', 'Object',
+                             ondelete='cascade', select=1)
     local_id = fields.Integer('Local ID', readonly=True)
     remote_id = fields.Integer('Remote ID', readonly=True)
 
