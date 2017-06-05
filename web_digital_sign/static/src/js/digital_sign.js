8ba797f031332a757c65e7cf5d78e6a514aa1458
Vishal Patel <vishal.patel.serpentcs@gmail.com>
2017-06-05 17:51:43 +0530

    Fixed travis ci log.

--- web_digital_sign/static/src/js/digital_sign.js
+++ web_digital_sign/static/src/js/digital_sign.js
@@ -38,7 +38,7 @@ odoo.define('web_digital_sign.web_digital_sign', function(require) {
             var self = this;
             this.$el.find('> img').remove();
             var signature = self.$el.find("#signature").jSignature("getData",'image');
-            var is_empty = signature ? 
+            var is_empty = signature ?
                     self.empty_sign[1] === signature[1] :
                         false;
             if (! is_empty && signature !== undefined && signature[1]) {
@@ -54,9 +54,7 @@ odoo.define('web_digital_sign.web_digital_sign', function(require) {
                 url = this.session.url('/web/binary/image', {
                     model: this.view.dataset.model,
                     id: JSON.stringify(this.view.datarecord.id || null),
-                    field: (this.options.preview_image)? 
-                            this.options.preview_image : 
-                            this.name,
+                    field:  this.options.preview_image ? this.options.preview_image : this.name,
                     t: (new Date().getTime())
                 });
             } else {
@@ -70,12 +68,11 @@ odoo.define('web_digital_sign.web_digital_sign', function(require) {
                 $img.load(function() {
                     if (! self.options.size) {
                         return;
-                    } else {
-                        $img.css("max-width", "" + self.options.size[0] + "px");
-                        $img.css("max-height", "" + self.options.size[1] + "px");
-                        $img.css("margin-left", "" + (self.options.size[0] - $img.width()) / 2 + "px");
-                        $img.css("margin-top", "" + (self.options.size[1] - $img.height()) / 2 + "px");
                     }
+                    $img.css("max-width", "" + self.options.size[0] + "px");
+                    $img.css("max-height", "" + self.options.size[1] + "px");
+                    $img.css("margin-left", "" + (self.options.size[0] - $img.width()) / 2 + "px");
+                    $img.css("margin-top", "" + (self.options.size[1] - $img.height()) / 2 + "px");
                 });
                 $img.on('error', function() {
                     $img.attr('src', self.placeholder);
@@ -84,9 +81,7 @@ odoo.define('web_digital_sign.web_digital_sign', function(require) {
             } else if (this.view.get("actual_mode") === 'edit') {
                 this.$el.find('> img').remove();
                 if (this.get('value')) {
-                    var field_name = this.options.preview_image ?
-                            this.options.preview_image :
-                            this.name;
+                    var field_name = this.options.preview_image ? this.options.preview_image : this.name;
                     new Model(this.view.dataset.model).call("read", [this.view.datarecord.id, [field_name]]).done(function(data) {
                         if (data) {
                             var field_desc = _.values(_.pick(data[0], field_name));
@@ -104,8 +99,7 @@ odoo.define('web_digital_sign.web_digital_sign', function(require) {
               this.$el.find('> img').remove();
               this.$el.find('> canvas').remove();
               if (!this.get('value')) {
-                  var sign_options = {'decor-color' : '#D1D0CE', 'color': '#000','background-color': '#fff','height':'150','width':'550'};
-                  this.$el.find(".signature").empty().jSignature("init",sign_options);
+                  this.$el.find(".signature").empty().jSignature("init",{'decor-color' : '#D1D0CE', 'color': '#000','background-color': '#fff','height':'150','width':'550'});
               }
           }
         },
