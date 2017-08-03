4d01347de0a15674f8c41b6f670214ae8f8d9517
Husen Daudi <husen.daudi@serpentcs.com>
2017-08-03 18:00:01 +0530

    fixed summary

--- sale_restrict/__manifest__.py
+++ sale_restrict/__manifest__.py
@@ -2,11 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "SO - Product Price Check",
+    'name': 'SO - Product Price Check',
     'version': '10.0.1.0.0',
     'category': 'Sales Management',
+    'summary': 'Restrict sales on 0 value.',
+    'description': """
+        This module restricts a user from confirming a Sale Order/Quotation
+        if it contains products having sale price zero.
+    """,
     'license': 'AGPL-3',
-    'author': "Serpent Consulting Services Pvt. Ltd.",
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
     'depends': ['sale'],
     'images': ['static/description/PriceCheck.png'],
