74f76abd5aa702ad5ac789f30e705fc135827aa1
Husen Daudi <husen.daudi@serpentcs.com>
2017-08-03 18:00:01 +0530

    fixed summary

--- hr_grade_rank/__manifest__.py
+++ hr_grade_rank/__manifest__.py
@@ -5,11 +5,13 @@
     'name': 'HR-Grade Rank',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
+    'summary' :'Manage grade and rank of employee',
     'description': """
         HR-Grade Rank
         =============
         This module is used to add grade of employees.""",
     'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'category': 'Human Resources',
     'website': 'https://www.serpentcs.com',
     'depends': ['hr'],
@@ -18,4 +20,6 @@
         'views/hr_grade_rank_view.xml',
     ],
     'images': ['static/description/HRGradeRank.png'],
+    'installable': True,
+    'auto_install': False,
 }
