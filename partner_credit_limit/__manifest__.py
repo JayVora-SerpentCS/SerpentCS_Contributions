fcd80df143a5a7f157bc138bd20f405b04ca7496
Husen Daudi <husen.daudi@serpentcs.com>
2017-08-03 18:00:01 +0530

    fixed summary

--- partner_credit_limit/__manifest__.py
+++ partner_credit_limit/__manifest__.py
@@ -8,6 +8,8 @@
     'depends': ['account', 'sale'],
     'license': 'AGPL-3',
     'author': 'Tiny, Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+    'summary': 'Set credit limit warning',
     'description': '''Partner Credit Limit'
         Checks for all over due payment and already paid amount
         if the difference is positive and acceptable then Salesman
