1d30f447ea3f40d924349e8dfebac7631d05a583
Hetal Zala <hetal.zala.serpentcs@gmail.com>
2017-09-15 11:07:35 +0530

    [FIX] Fixed Travis CI logs.

--- sale_restrict/__openerp__.py
+++ sale_restrict/__openerp__.py
@@ -5,10 +5,6 @@
     'name': "SO - Product Price Check",
     'version': '9.0.1.1.0',
     'category': 'Sales Management',
-    'description': """
-        This module restricts a user from confirming a Sale Order/Quotation
-        if it contains products having sale price zero.
-    """,
     'author': "Serpent Consulting Services Pvt. Ltd.",
     'website': "http://www.serpentcs.com",
     'depends': ['sale'],
