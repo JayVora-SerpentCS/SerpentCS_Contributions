21743a44c97033e2521a280c2b78072f3f0e0ce1
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-10-25 17:40:22 +0530

    Migrated partner_credit_limit module in v11.

--- partner_credit_limit/__manifest__.py
+++ partner_credit_limit/__manifest__.py
@@ -3,11 +3,11 @@
 
 {
     'name': 'Partner Credit Limit',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Partner',
-    'depends': ['account', 'sale'],
     'license': 'AGPL-3',
     'author': 'Tiny, Serpent Consulting Services Pvt. Ltd.',
+    'website': 'http://www.serpentcs.com',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'summary': 'Set credit limit warning',
     'description': '''Partner Credit Limit'
@@ -15,10 +15,11 @@
         if the difference is positive and acceptable then Salesman
         able to confirm SO
     ''',
-    'website': 'http://www.serpentcs.com',
+    'depends': [
+        'sale',
+    ],
     'data': [
         'views/partner_view.xml',
     ],
     'installable': True,
-    'auto_install': False,
 }
