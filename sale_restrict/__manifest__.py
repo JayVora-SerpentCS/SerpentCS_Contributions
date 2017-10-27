cd8c31e6939a161a1a0528738b4809f63e70491e
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-10-27 18:18:52 +0530

    [ADD] Migrated sale_restrict module in v11.

--- sale_restrict/__manifest__.py
+++ sale_restrict/__manifest__.py
@@ -3,19 +3,22 @@
 
 {
     'name': 'SO - Product Price Check',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Sales Management',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
     'summary': 'Restrict sales on 0 value.',
     'description': """
         This module restricts a user from confirming a Sale Order/Quotation
         if it contains products having sale price zero.
     """,
     'license': 'AGPL-3',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
-    'depends': ['sale'],
-    'images': ['static/description/PriceCheck.png'],
+    'depends': [
+        'sale',
+    ],
+    'images': [
+        'static/description/PriceCheck.png',
+    ],
     'installable': True,
-    'auto_install': False,
 }
