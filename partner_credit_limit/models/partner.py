3a69a2e675f0c32a7c8fece2dbe73eafb5ea2672
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-10-27 17:37:51 +0530

    Changed the years in licensing terms.

--- partner_credit_limit/models/partner.py
+++ partner_credit_limit/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016-TODAY Serpent Consulting Services Pvt. Ltd.
 # See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
