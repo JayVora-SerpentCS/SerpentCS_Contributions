ea1bf2acdf0781e78931d290fe8a86d180aa93f5
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-11-01 17:34:12 +0530

    [ADD] Migrated project_team module in v11.

--- project_team/__manifest__.py
+++ project_team/__manifest__.py
@@ -8,15 +8,19 @@
     'summary': 'Project Team Management',
     'category': 'Project Management',
     'website': 'http://www.serpentcs.com',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
-    'sequence': 1,
-    'depends': ['project', 'crm', 'web'],
+    'depends': [
+        'project',
+        'crm',
+        'web',
+    ],
     'data': [
-        'views/template.xml',
+#        'views/template.xml',
         'views/project_team_view.xml',
     ],
-    'images': ['static/description/ProjectTeam.png'],
+    'images': [
+        'static/description/ProjectTeam.png',
+    ],
     'installable': True,
-    'auto_install': False
 }
---
+++ project_team/models/crm_team.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class CrmTeamInherit(models.Model):
+    _inherit = 'crm.team'
+
+    type_team = fields.Selection([('sale', 'Sale'), ('project', 'Project')],
+                                 "Type", default="sale")
+    team_members = fields.Many2many('res.users', 'project_team_user_rel',
+                                    'team_id', 'uid', 'Project Members',
+                                    help="""Project's members are users who
+                                     can have an access to the tasks related
+                                     to this project.""")
