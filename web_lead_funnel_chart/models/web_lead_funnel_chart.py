587ec899ac194214961f1c3d5017863987e7fb95
Jagdish Panchal <jagdish.panchal@serpentcs.com>
2017-11-02 15:54:49 +0530

    [IMP] Improved code as per suggested

--- web_lead_funnel_chart/models/web_lead_funnel_chart.py
+++ web_lead_funnel_chart/models/web_lead_funnel_chart.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class Crmleadextended(models.Model):
@@ -8,15 +8,19 @@ class Crmleadextended(models.Model):
 
     @api.multi
     def get_lead_stage_data(self):
-        stage_ids = self.env['crm.stage'].search([])
+        self._cr.execute('select id from crm_stage')
+        stage_ids = [stage_id[0] for stage_id in self._cr.fetchall()]
         crm_lst = []
-        for stage in stage_ids:
-            self._cr.execute('select count(id) from crm_lead \
-                where stage_id =%s', (stage.id,))
-            leads = self._cr.fetchone()
-            crm_lst.append((stage.name, int(leads and leads[0])))
+        stages = self.env['crm.stage'].browse(stage_ids)
+        for stage in stages:
+            if stage:
+                self._cr.execute('select count(id) from crm_lead \
+                where stage_id =%s', (stage.id, ))
+                leads = self._cr.fetchone()
+                crm_lst.append((stage.name, int(leads and leads[0] or 0)))
         return crm_lst
 
+    #This method is overridden for archiving stage of funnel chart
     @api.multi
     def write(self, vals):
         active = vals.get('active', False)
@@ -24,8 +28,14 @@ class Crmleadextended(models.Model):
         stage_id = self.stage_id
         if active:
             stage_id = ir_model.get_object('crm', 'stage_lead1')
-        elif not active:
+        else:
             stage_id = ir_model.get_object('web_lead_funnel_chart',
                                            'stage_lead_archive')
         vals.update({'stage_id': stage_id.id})
         return super(Crmleadextended, self).write(vals)
+
+
+class StageExtended(models.Model):
+    _inherit = 'crm.stage'
+
+    active = fields.Boolean('Active', default=True)
