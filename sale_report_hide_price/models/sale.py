7f5e7315d2fbea3c5f847a9436109a41566c25a7
Sudhir Arya <sudhir.arya@serpentcs.com>
2017-11-03 18:14:12 +0530

    Migrated sale_report_hide_price module in v11.

--- sale_report_hide_price/models/sale.py
+++ sale_report_hide_price/models/sale.py
@@ -2,16 +2,15 @@
 # © 2016 Serpent Consulting Services Pvt. Ltd. <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    show_price = fields.Boolean('Show Price', help="If checked, you can see"
-                                                   " the price in report of"
-                                                   "Sales Order / Quotation.")
+    show_price = fields.Boolean('Show Price',
+                                help="If checked, you can see the price in "
+                                     "report of" "Sales Order / Quotation.")
     show_discount = fields.Boolean('Show Discount',
                                    help="If checked, you can see the discount"
-                                        " in report of Sales Order /"
-                                        " quotation.")
+                                        " in report of Sales Order/Quotation.")
