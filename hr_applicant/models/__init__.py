75b3eb951ffa229aa7bd2df3e776a0c33d8eefba
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2017-11-08 18:04:27 +0530

    [IMP] Improved the license terms in the files.

--- hr_applicant/models/__init__.py
+++ hr_applicant/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# See LICENSE file for full copyright and licensing details.
 
-import hr_recruitment
-import hr_recruitment_employee
-import training
+from . import hr_recruitment
+from . import hr_recruitment_employee
+from . import training
