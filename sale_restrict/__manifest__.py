0610d1131d4a9d1c1ac372e1af34af04ebd5970f
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2018-01-25 15:17:04 +0530

    [IMP] add solve travis error

--- sale_restrict/__manifest__.py
+++ sale_restrict/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 {
@@ -5,13 +6,11 @@
     'version': '11.0.1.0.0',
     'category': 'Sales Management',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'summary': '''Restrict sales on 0 value.
-                  This module restricts a user from
-                  confirming a Sale Order/Quotation.
-                  This module restricts a user from
-                  confirming a Sale Order/Quotation
-                  if it contains products having sale price Zero.
-                ''',
+    'summary': 'Restrict sales on 0 value.',
+    'description': """
+        This module restricts a user from confirming a Sale Order/Quotation
+        if it contains products having sale price zero.
+    """,
     'license': 'AGPL-3',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
