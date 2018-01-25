8febab6e3ca846e7ebc1452b96da6265d0d18693
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2018-01-25 14:56:16 +0530

    [IMP]made improvements, fix travis errors

--- hr_applicant/models/hr_recruitment.py
+++ hr_applicant/models/hr_recruitment.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime
@@ -550,9 +549,9 @@ class ApplicantLanguage(models.Model):
         'hr.applicant', 'Applicant Ref', ondelete='cascade')
     mother_tongue = fields.Boolean('Mother Tongue')
 
-    @api.one
     @api.constrains('mother_tongue')
     def _check_mother_tongue(self):
+        self.ensure_one()
         if self.mother_tongue and self.applicant_id:
             language_rec = self.search([
                 ('applicant_id', '=', self.applicant_id.id),
