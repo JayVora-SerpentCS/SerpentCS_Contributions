e011baa3bb6032a23a5325935c0acd5d8756f729
arpit.joshi.serpentcs@gmail.com <Arpit Joshi>
2018-01-25 17:04:12 +0530

    [IMP]improvements in readme rst and stringio

--- base_module_record/wizard/base_module_save.py
+++ base_module_record/wizard/base_module_save.py
@@ -27,7 +27,7 @@ def _create_module(self, ids):
     res_xml = mod.generate_xml()
     ids = self.search([('id', 'in', ids)])
     data = ids.read([])[0]
-    s = StringIO()
+    s = StringIO.StringIO()
     zip_file = zipfile.ZipFile(s, 'w')
     dname = data['directory_name']
     data['update_name'] = ''
