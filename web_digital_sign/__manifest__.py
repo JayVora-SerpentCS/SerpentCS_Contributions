83c498bdd382d13057fa057f6421f8dad7a3b704
arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
2018-01-30 12:34:30 +0530

    improvements in manifest file

--- web_digital_sign/__manifest__.py
+++ web_digital_sign/__manifest__.py
@@ -9,20 +9,6 @@
     'depends': ['web'],
     "license": "AGPL-3",
     'summary': 'Digital signature control',
-#    'description': '''
-#     This module provides the functionality to store digital signature
-#     for a record.
-#        -> This  module is helpfull to make your business process a little
-#           bit more faster & makes it more user friendly by providing you
-#           digital signature functionality on your documents.
-#        -> It is touch screen enable so user can add signature with touch
-#           devices.
-#        -> Digital signature can be very usefull for documents such as
-#           sale orders, purchase orders, inovoices, payslips, procurement
-#           receipts, etc.
-#        The example can be seen into the User's form view where we have
-#        added a test field under signature.
-#    ''',
     'data': [
         'views/we_digital_sign_view.xml',
         'views/users_view.xml'],
