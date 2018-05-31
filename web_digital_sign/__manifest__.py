1ce119cc6cb8d84f161c72e40a3a669e126f89c3
Nisha Gajjar <n.gajjar.serpentcs@gmail.com>
2018-05-31 15:50:21 +0530

    [PATCH 38/48] improved description

--- web_digital_sign/__manifest__.py
+++ web_digital_sign/__manifest__.py
@@ -9,17 +9,34 @@
     'depends': ['web'],
     "license": "AGPL-3",
      'category': 'Tools',
-    'description': """
-        Digital signature control
-        ==========================
-        Digital signature,
-        signature
-    """,
-    'summary': """Digital signature control
-    ==============================
-    Digital signature
-    signature
-    """,
+    'description': '''
+     This module provides the functionality to store digital signature
+     for a record.
+        -> This  module is helpfull to make your business process a little
+           bit more faster & makes it more user friendly by providing you
+           digital signature functionality on your documents.
+        -> It is touch screen enable so user can add signature with touch
+           devices.
+        -> Digital signature can be very usefull for documents such as
+           sale orders, purchase orders, invoices, payslips, procurement
+           receipts, etc.
+        The example can be seen into the User's form view where we have
+        added a test field under signature.
+    ''',
+    'summary': '''
+     This module provides the functionality to store digital signature
+     for a record.
+        -> This  module is helpfull to make your business process a little
+           bit more faster & makes it more user friendly by providing you
+           digital signature functionality on your documents.
+        -> It is touch screen enable so user can add signature with touch
+           devices.
+        -> Digital signature can be very usefull for documents such as
+           sale orders, purchase orders, invoices, payslips, procurement
+           receipts, etc.
+        The example can be seen into the User's form view where we have
+        added a test field under signature.
+    ''',
     'images': ['static/description/Digital_Signature.jpg'],
     'data': [
         'views/we_digital_sign_view.xml',
