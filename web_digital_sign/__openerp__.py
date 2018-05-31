fbf89ff1bebb6457dc21d68bea9560f621f27199
Nisha Gajjar <n.gajjar.serpentcs@gmail.com>
2018-05-31 18:50:43 +0530

    [IMP]improved index file and modified screen shot according version 9

--- web_digital_sign/__openerp__.py
+++ web_digital_sign/__openerp__.py
@@ -4,9 +4,37 @@
     "name": "Web Digital Signature 9.0",
     "version": "9.0.1.0.0",
     'author': 'Serpent Consulting Services Pvt. Ltd.',
-    "category": '',
+    "category": 'Tools',
     'license': 'AGPL-3',
     'complexity': "easy",
+    'description': '''
+     This module provides the functionality to store digital signature
+     for a record.
+        -> This  module is helpfull to make your business process a little
+           bit more faster & makes it more user friendly by providing you
+           digital signature functionality on your documents.
+        -> It is touch screen enable so user can add signature with touch
+           devices.
+        -> Digital signature can be very usefull for documents such as
+           sale orders, purchase orders, invoices, payslips, procurement
+           receipts, etc.
+        The example can be seen into the User's form view where we have
+        added a test field under signature.
+    ''',
+    'summary': '''
+     This module provides the functionality to store digital signature
+     for a record.
+        -> This  module is helpfull to make your business process a little
+           bit more faster & makes it more user friendly by providing you
+           digital signature functionality on your documents.
+        -> It is touch screen enable so user can add signature with touch
+           devices.
+        -> Digital signature can be very usefull for documents such as
+           sale orders, purchase orders, invoices, payslips, procurement
+           receipts, etc.
+        The example can be seen into the User's form view where we have
+        added a test field under signature.
+    ''',
     'depends': ['web'],
     'data': [
         'views/we_digital_sign_view.xml',
