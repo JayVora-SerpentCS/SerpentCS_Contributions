a10ccf8ba3d12e304f4de836b53722a8635d4c6e
Chankya <chanakya.soni@serpentcs.com>
2018-11-23 17:18:23 +0530

    [12.0][MIG] Migrated sale_product_smart_buttons module in v12

--- sale_product_smart_buttons/models/product.py
+++ sale_product_smart_buttons/models/product.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
 
 
-class product_template(models.Model):
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
