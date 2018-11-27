533674bef95a318aa4baf2d0529376b6c1e97ba4
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2018-11-27 18:45:27 +0530

    [MIG][12.0]import_export_templating: Migrated.

--- import_export_templating/__manifest__.py
+++ import_export_templating/__manifest__.py
@@ -1,31 +1,24 @@
-# -*- coding: utf-8 -*-
 # See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Import / Export Templating',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'category': 'Base',
-    'description': """
-        This module features to export blank template based on ir.model fields.
-        & provided .xls extension file to import!
-    """,
-    'summary': """
-    Standard import export template
-    USer friendly templates to upload download bulk data
-    Manage easy import export for non-technical users of ERP
-    """,
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
-    'depends': ['base', 'web'],
+    'category': 'Base',
+    'summary': """
+    Standard import export template,
+    User-friendly templates to upload download bulk data Manage easy
+    import-export for non-technical users of ERP.
+    """,
+    'depends': ['web'],
     'data': [
         'wizard/wiz_download_template_view.xml',
         'views/templates.xml',
     ],
-    'application': False,
     'installable': True,
-    'auto_install': False,
     'price': 30.0,
     'currency': 'EUR',
 }
