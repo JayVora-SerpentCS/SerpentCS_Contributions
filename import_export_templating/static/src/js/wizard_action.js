f34e30afec4261b2eaedf799886790dc564bfa26
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2018-11-27 18:45:27 +0530

    import_export_templating: Migrated.

--- import_export_templating/static/src/js/wizard_action.js
+++ import_export_templating/static/src/js/wizard_action.js
@@ -6,7 +6,6 @@ var FormController = require('web.FormController');
 var ListController = require('web.ListController');
 var ActionManager = require('web.ActionManager');
 var Dialog = require('web.Dialog');
-var ViewManager = require('web.ViewManager');
 var dom = require('web.dom');
 var core = require('web.core');
 var Sidebar = require('web.Sidebar');
@@ -127,141 +126,104 @@ ListController.include({
 
 });
 
-
 ActionManager.include({
+    /**
+     * Executes actions with attribute target='new'. Such actions are rendered
+     * in a dialog.
+     *
+     * @private
+     * @param {Object} action
+     * @param {Object} options @see doAction for details
+     * @returns {Deferred} resolved when the controller is rendered inside a
+     *   dialog appended to the DOM
+     */
+    _executeActionInDialog: function (action, options) {
+        var self = this;
+        var controller = this.controllers[action.controllerID];
+        var widget = controller.widget;
+        // AAB: this will be moved to the Controller
+        if (widget.need_control_panel) {
+            // set the ControlPanel bus on the controller to allow it to
+            // communicate its status
+            widget.set_cp_bus(new Bus());
+        }
 
-    ir_actions_common: function(executor, options) {
-            var self = this;
-            if (executor.action.target === 'new') {
-                var pre_dialog = (this.dialog && !this.dialog.isDestroyed()) ? this.dialog : null;
-                var flag = true;
-                if (pre_dialog){
-                    // prevent previous dialog to consider itself closed,
-                    // right now, as we're opening a new one (prevents
-                    // reload of original form view)
-                    pre_dialog.off('closed', null, pre_dialog.on_close);
+        return this._startController(controller).then(function (controller) {
+            var prevDialogOnClose;
+            if (self.currentDialogController) {
+                if (action.context['nodestroy']) {
+                    self.precurrentDialogController = self.currentDialogController;
+                    prevDialogOnClose = self.currentDialogController.onClose;
+                }else {
+                    prevDialogOnClose = self.currentDialogController.onClose;
+                    self._closeDialog({ silent: true });
                 }
-                if (this.dialog_widget && !this.dialog_widget.isDestroyed()) {
-                    if(executor.action && executor.action.context  &&
-                        executor.action.context['nodestroy']){
-                        flag = false;
-                        self.prev_dialog = self.dialog
-                        self.prev_dialog_widget = self.dialog_widget
-                    }else{
-                        this.dialog_widget.destroy();
-                    }
-                }
-                // explicitly passing a closing action to dialog_stop() prevents
-                // it from reloading the original form view
-
-                if(flag){
-                    this.dialog_stop(executor.action);
-                }
-                
-                this.dialog = new Dialog(this, _.defaults(options || {}, {
-                    title: executor.action.name,
-                    dialogClass: executor.klass,
-                    buttons: [],
-                    size: executor.action.context.dialog_size,
-                }));
-
-                // chain on_close triggers with previous dialog, if any
-                this.dialog.on_close = function(){
-                    options.on_close.apply(null, arguments);
-                    if (pre_dialog && pre_dialog.on_close){
-                        // no parameter passed to on_close as this will
-                        // only be called when the last dialog is truly
-                        // closing, and *should* trigger a reload of the
-                        // underlying form view (see comments above)
-                        pre_dialog.on_close();
-                    }
-                    if (!pre_dialog) {
-                        self.dialog = null;
-                    }
-                };
-                this.dialog.on("closed", null, this.dialog.on_close);
-                this.dialog_widget = executor.widget();
-                var $dialogFooter;
-                if (this.dialog_widget instanceof ViewManager) {
-                    executor.action.viewManager = this.dialog_widget;
-                    $dialogFooter = $('<div/>'); // fake dialog footer in which view
-                                                 // manager buttons will be put
-                    _.defaults(this.dialog_widget.flags, {
-                        $buttons: $dialogFooter,
-                        footer_to_buttons: true,
-                    });
-                    if (this.dialog_widget.action.view_mode === 'form') {
-                        this.dialog_widget.flags.headless = true;
-                    }
-                }
-                if (this.dialog_widget.need_control_panel) {
-                    // Set a fake bus to Dialogs needing a ControlPanel as they should not
-                    // communicate with the main ControlPanel
-                    this.dialog_widget.set_cp_bus(new Bus());
-                }
-                this.dialog_widget.setParent(this.dialog);
-
-                var fragment = document.createDocumentFragment();
-                return this.dialog_widget.appendTo(fragment).then(function () {
-                    var def = $.Deferred();
-                    self.dialog.opened().then(function () {
-                        dom.append(self.dialog.$el, fragment, {
-                            in_DOM: true,
-                            callbacks: [{widget: self.dialog_widget}],
-                        });
-                        if ($dialogFooter) {
-                            self.dialog.$footer.empty().append($dialogFooter.contents());
-                        }
-                        if (options.state && self.dialog_widget.do_load_state) {
-                            return self.dialog_widget.do_load_state(options.state);
-                        }
-                    })
-                    .done(def.resolve.bind(def))
-                    .fail(def.reject.bind(def));
-                    self.dialog.open();
-                    return def;
-                }).then(function () {
-                    return executor.action;
-                });
             }
-            var def = this.inner_action && this.webclient && this.webclient.clear_uncommitted_changes() || $.when();
-            return def.then(function() {
-                self.dialog_stop(executor.action);
-                return self.push_action(executor.action.viewManager = executor.widget(), executor.action, options).then(function () {
-                    return executor.action;
-                });
-            }).fail(function() {
-                return $.Deferred().reject();
+
+            controller.onClose = prevDialogOnClose || options.on_close;
+            var dialog = new Dialog(self, _.defaults({}, options, {
+                buttons: [],
+                dialogClass: controller.className,
+                title: action.name,
+                size: action.context.dialog_size,
+            }));
+            /**
+             * @param {Object} [options={}]
+             * @param {Object} [options.infos] if provided and `silent` is
+             *   unset, the `on_close` handler will pass this information,
+             *   which gives some context for closing this dialog.
+             * @param {boolean} [options.silent=false] if set, do not call the
+             *   `on_close` handler.
+             */
+            dialog.on('closed', self, function (options) {
+                options = options || {};
+                self._removeAction(action.jsID);
+                self.currentDialogController = null;
+                if (options.silent !== true) {
+                    controller.onClose(options.infos);
+                }
             });
-        },
+            controller.dialog = dialog;
 
-    ir_actions_act_window_close: function (action, options) {
-        if (!this.dialog) {
-            options.on_close();
-        }
-        if(action && action.context && action.context.close_previous_dialog){
-            if(this.prev_dialog){
-                this.prev_dialog_widget.destroy()
-                this.prev_dialog.destroy()
-                this.prev_dialog = null
-            }
-            if(this.dialog){
-                this.dialog_stop();
-            }
-        }else{ 
-            this.dialog_stop();
+            return dialog.open().opened(function () {
+                self.currentDialogController = controller;
 
+                dom.append(dialog.$el, widget.$el, {
+                    in_DOM: true,
+                    callbacks: [{widget: dialog}, {widget: controller.widget}],
+                });
+                widget.renderButtons(dialog.$footer);
+                dialog.rebindButtonBehavior();
+
+                return action;
+            });
+        }).fail(function () {
+            self._removeAction(action.jsID);
+        });
+    },
+    _executeCloseAction: function (action, options) {
+        var result;
+        if (!this.currentDialogController) {
+            result = options.on_close(action.infos);
+            if(action && action.context && action.context.close_previous_dialog){
+                if(this.precurrentDialogController){
+                    if (this.precurrentDialogController) {
+                        this.precurrentDialogController.dialog.destroy(options);
+                        this.precurrentDialogController = null;
+                    }
+                }
+            }
         }
-        
-        // Display rainbowman on appropriate actions
+
+        this._closeDialog({ infos: action.infos });
+
+        // display some effect (like rainbowman) on appropriate actions
         if (action.effect) {
             this.trigger_up('show_effect', action.effect);
         }
 
-        return $.when();
+        return $.when(result);
     },
-
 });
-
 
 });
