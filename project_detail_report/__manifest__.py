4de5f3a908190cc4d584af451e06f94b4cd2f8ad
Dhirendrasinh <d.thakor.serpentcs@gmail.com>
2018-11-28 13:26:14 +0530

    [MIG]Migrated project_detail_report v12

--- project_detail_report/__manifest__.py
+++ project_detail_report/__manifest__.py
@@ -2,9 +2,10 @@
 
 {
     'name': 'Project Report',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Project Management',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'license': 'AGPL-3',
     'website': 'http://www.serpentcs.com',
     'summary': """
       Print Project Detail report with task list and task details.""",
@@ -12,7 +13,6 @@
     'depends': [
         'hr_timesheet',
     ],
-    "license": "AGPL-3",
     'data': [
         'views/project_report.xml',
         'report/project_qweb_report.xml',
