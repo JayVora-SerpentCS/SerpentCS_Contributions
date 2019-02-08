d4521b59fa7e4198ec0a13905ec374857b1065fb
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2019-02-08 17:30:12 +0530

    Fixed the issue of report method for label module.

--- label/__manifest__.py
+++ label/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Mass Label Reporting",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
     "summary": "Generate customised labels of any document",
