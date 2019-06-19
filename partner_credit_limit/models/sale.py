fcd937bb6a0a39c22edf0a793f74ff1a30b7ef98
Vishal Mangroliya <vishal.m.serpentcs@gmail.com>
2019-06-19 18:48:09 +0530

    partner_credit_limit: Improved code.

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -12,33 +12,41 @@ class SaleOrder(models.Model):
     def check_limit(self):
         self.ensure_one()
         partner = self.partner_id
-        moveline_obj = self.env['account.move.line']
-        movelines = moveline_obj.search(
-            [('partner_id', '=', partner.id),
-             ('account_id.user_type_id.name', 'in', ['Receivable', 'Payable'])]
-        )
-        confirm_sale_order = self.search([('partner_id', '=', partner.id),
-                                          ('state', '=', 'sale')])
-        debit, credit = 0.0, 0.0
-        amount_total = 0.0
-        for status in confirm_sale_order:
-            amount_total += status.amount_total
-        for line in movelines:
-            credit += line.credit
-            debit += line.debit
-        partner_credit_limit = (partner.credit_limit - debit) + credit
-        available_credit_limit = ((partner_credit_limit -
-                                   (amount_total - debit)) + self.amount_total)
+        user_id = self.env['res.users'].search([
+            ('partner_id', '=', partner.id)], limit=1)
+        if user_id and not user_id.has_group('base.group_portal') or not \
+                user_id:
+            moveline_obj = self.env['account.move.line']
+            movelines = moveline_obj.search(
+                [('partner_id', '=', partner.id),
+                 ('account_id.user_type_id.name', 'in',
+                  ['Receivable', 'Payable'])]
+            )
+            confirm_sale_order = self.search([('partner_id', '=', partner.id),
+                                              ('state', '=', 'sale')])
+            debit, credit = 0.0, 0.0
+            amount_total = 0.0
+            for status in confirm_sale_order:
+                amount_total += status.amount_total
+            for line in movelines:
+                credit += line.credit
+                debit += line.debit
+            partner_credit_limit = (partner.credit_limit - debit) + credit
+            available_credit_limit = \
+                ((partner_credit_limit -
+                  (amount_total - debit)) + self.amount_total)
 
-        if (amount_total - debit) > partner_credit_limit:
-            if not partner.over_credit:
-                msg = 'Your available credit limit'\
-                      ' Amount = %s \nCheck "%s" Accounts or Credit ' \
-                      'Limits.' % (available_credit_limit,
-                                   self.partner_id.name)
-                raise UserError(_('You can not confirm Sale Order. \n' + msg))
-            partner.write({'credit_limit': credit - debit + self.amount_total})
-        return True
+            if (amount_total - debit) > partner_credit_limit:
+                if not partner.over_credit:
+                    msg = 'Your available credit limit' \
+                          ' Amount = %s \nCheck "%s" Accounts or Credit ' \
+                          'Limits.' % (available_credit_limit,
+                                       self.partner_id.name)
+                    raise UserError(_('You can not confirm Sale '
+                                      'Order. \n' + msg))
+                partner.write(
+                    {'credit_limit': credit - debit + self.amount_total})
+            return True
 
     @api.multi
     def action_confirm(self):
