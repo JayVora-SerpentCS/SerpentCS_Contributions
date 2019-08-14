d962e81966fa841644cc110493f9527affec0667
Bansari Mistry <bansari.mistry.serpentcs@gmail.com>
2019-08-14 15:23:34 +0530

    [IMP] base_synchro: Improved code and fixed error.

--- base_synchro/models/res_request.py
+++ base_synchro/models/res_request.py
@@ -8,17 +8,8 @@ class ResRequest(models.Model):
     _order = 'date desc'
     _description = 'Request'
 
-    name = fields.Char(
-        string='Subject',
-        required=True
-    )
-    date = fields.Datetime(string='Date')
-    act_from = fields.Many2one(
-        'res.users',
-        string='From'
-    )
-    act_to = fields.Many2one(
-        'res.users',
-        string='To'
-    )
+    name = fields.Char('Subject', required=True)
+    date = fields.Datetime()
+    act_from = fields.Many2one('res.users', 'From')
+    act_to = fields.Many2one('res.users', 'To')
     body = fields.Text(string='Request')
