c0a8b3acee7bcb2e97d0f077a3c0ee9b1c9b37aa
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2019-10-05 02:53:35 +0530

    [MIG] sale_restrict module mmigrated in v13.

--- sale_restrict/models/sale_order.py
+++ sale_restrict/models/sale_order.py
@@ -1,16 +1,15 @@
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models, _
+from odoo import models, _
 from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    @api.multi
     def action_confirm(self):
         zero_price = [x.product_id.name
-                      for x in self.order_line if not x.price_unit]
+                      for x in self.order_line if not x.price_unit > 0.0]
 
         if zero_price:
             message = _("Please specify unit price for "
