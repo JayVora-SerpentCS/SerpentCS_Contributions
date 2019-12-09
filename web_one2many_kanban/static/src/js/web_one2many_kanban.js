29c0239f4c864d2c53c94f491de114853a01538f
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2019-12-09 10:59:56 +0530

    [MIG]migrated the module to v13 and fixed the issue of wiping out data when reloaded from mobile device.

--- web_one2many_kanban/static/src/js/web_one2many_kanban.js
+++ web_one2many_kanban/static/src/js/web_one2many_kanban.js
@@ -7,14 +7,15 @@ var KanbanRecord = require('web.KanbanRecord');
 
 KanbanRecord.include({
     _render: function () {
-        var def = $.Deferred();
         var self = this;
+        var def =this._super.apply(this, arguments)
         var o2x_field_names = [];
         _.each(this.fieldsInfo, function (field_info, field_nm){
-            if(field_info.mode === 'list'){
+            if(field_info.mode === 'list' || field_info.mode === 'kanban'){
                 o2x_field_names.push(field_nm);
             }
         })
+
         if( o2x_field_names.length > 0){
             var o2x_records = [];
             _.each(o2x_field_names,function(o2x_field_name){
@@ -25,18 +26,15 @@ KanbanRecord.include({
             });
             def = ajax.jsonRpc(
                         "/web/fetch_x2m_data",
-                        "call", 
+                        "call",
                         {'o2x_records': o2x_records}
-                  ).done(function(o2x_datas) {
+                  ).then(function(o2x_datas) {
                       for(var i=0;i<o2x_datas.length;i++){
                           o2x_records[i].raw_value = o2x_datas[i];
                       }
                   });
         }
-        else{
-            def.resolve();
-        }
-        def.done(function (){
+        return def.then(function (){
             self._replaceElement(self.qweb.render('kanban-box', self.qweb_context));
             self.$el.addClass('o_kanban_record');
             self.$el.data('record', self);
