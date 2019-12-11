453c291f3f5aedd043c56f3c381b303cc6014314
Vrushali <vrushali.k.serpentcs@gmail.com>
2019-12-11 15:25:51 +0530

    Migrated project detail report module.

--- project_detail_report/__manifest__.py
+++ project_detail_report/__manifest__.py
@@ -1,21 +1,29 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
+    # Module Information
     'name': 'Project Report',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Project Management',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
     'license': 'AGPL-3',
+    'summary': """Print Project Detail report with task
+                    list and task details.""",
+
+    # Author
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
-    'summary': """
-      Print Project Detail report with task list and task details.""",
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+
+    # Dependencies
     'depends': [
         'hr_timesheet',
     ],
+    # Data
     'data': [
         'views/project_report.xml',
         'report/project_qweb_report.xml',
     ],
+
+    # Technical
     'installable': True,
 }
