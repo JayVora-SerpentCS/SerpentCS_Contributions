7e399eb846d6c738c908aaf6f529612ee013d7e5
Vrushali <vrushali.k.serpentcs@gmail.com>
2019-12-12 18:53:07 +0530

    [IMP] Improved manifest file.

--- city/__manifest__.py
+++ city/__manifest__.py
@@ -1,11 +1,11 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "City-Helps to keep Homogeneous address data in the Database",
+    "name": "City - Helps to keep Homogeneous address data in the Database",
     "version": "13.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
-    "summary": "City-Helps to keep Homogeneous address data in the Database",
+    "summary": "City - Helps to keep Homogeneous address data in the Database",
     "author": "Pablo Rocandio, Serpent Consulting Services Pvt. Ltd.",
     "website": "http://www.serpentcs.com",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
