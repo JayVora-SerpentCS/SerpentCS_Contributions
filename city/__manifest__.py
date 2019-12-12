cdbbc2a49abf7a057b4625515419a998beab19d0
Vrushali <vrushali.k.serpentcs@gmail.com>
2019-12-12 15:15:58 +0530

    [IMP] Improved manifest file.

--- city/__manifest__.py
+++ city/__manifest__.py
@@ -1,28 +1,19 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    # Module Information
     "name": "City-Helps to keep Homogeneous address data in the Database",
     "version": "13.0.1.0.0",
     "category": "Hidden",
     "license": "AGPL-3",
     "summary": "City-Helps to keep Homogeneous address data in the Database",
-
-    # Author
     "author": "Pablo Rocandio, Serpent Consulting Services Pvt. Ltd.",
     "website": "http://www.serpentcs.com",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
-
-    # Dependencies
     "depends": ["sales_team", "crm"],
-
-    # Data
     "data": [
         "views/city_view.xml",
         "security/ir.model.access.csv",
     ],
-
-    # Technical
     "installable": True,
     "auto_install": False,
 }
