aa5d8bb86c12250e97bc9c0470bfbca6ac4abdc4
Drashti Patel - SerpentCS <d.patel.serpentcs@gmail.com>
2020-03-03 14:41:52 +0530

    [FIX][12.0]Fixed the issue of web_one2many_kanban related to access of accounting module.

--- web_one2many_kanban/static/src/js/web_one2many_kanban.js
+++ web_one2many_kanban/static/src/js/web_one2many_kanban.js
@@ -7,8 +7,9 @@ var KanbanRecord = require('web.KanbanRecord');
 
 KanbanRecord.include({
     _render: function () {
-        var def = $.Deferred();
         var self = this;
+        var def_val = $.Deferred();
+
         var o2x_field_names = [];
         _.each(this.fieldsInfo, function (field_info, field_nm){
             if(field_info.mode === 'list' || field_info.mode == 'kanban'){
@@ -23,9 +24,9 @@ KanbanRecord.include({
                     o2x_records.push(record);
                 }
             });
-            def = ajax.jsonRpc(
+            def_val = ajax.jsonRpc(
                         "/web/fetch_x2m_data",
-                        "call", 
+                        "call",
                         {'o2x_records': o2x_records}
                   ).done(function(o2x_datas) {
                       for(var i=0;i<o2x_datas.length;i++){
@@ -34,23 +35,31 @@ KanbanRecord.include({
                   });
         }
         else{
-            def.resolve();
+            def_val.resolve();
         }
-        def.done(function (){
-            self._replaceElement(self.qweb.render('kanban-box', self.qweb_context));
-            self.$el.addClass('o_kanban_record');
-            self.$el.data('record', self);
-            if (self.$el.hasClass('oe_kanban_global_click') ||
-                self.$el.hasClass('oe_kanban_global_click_edit')) {
-                self.$el.on('click', self._onGlobalClick.bind(self));
-            }
-            self._processFields();
-            self._processWidgets();
-            self._setupColor();
-            self._setupColorPicker();
-            self._attachTooltip();
-            // We use boostrap tooltips for better and faster display
-            self.$('span.o_tag').tooltip({delay: {'show': 50}});
+        return def_val.then(function (){
+                self.defs = [];
+                self._replaceElement(self.qweb.render('kanban-box', self.qweb_context));
+                self.$el.addClass('o_kanban_record').attr("tabindex",0);
+                self.$el.attr('role', 'article');
+                self.$el.data('record', self);
+                if (self.$el.hasClass('oe_kanban_global_click') ||
+                    self.$el.hasClass('oe_kanban_global_click_edit')) {
+                    self.$el.on('click', self._onGlobalClick.bind(self));
+                    self.$el.on('keydown', self._onKeyDownCard.bind(self));
+                } else {
+                    self.$el.on('keydown', self._onKeyDownOpenFirstLink.bind(self));
+                }
+                self._processFields();
+                self._processWidgets();
+                self._setupColor();
+                self._setupColorPicker();
+                self._attachTooltip();
+
+                // We use boostrap tooltips for better and faster display
+                self.$('span.o_tag').tooltip({delay: {'show': 50}});
+
+                return $.when.apply(self, self.defs);
         });
     },
 })
