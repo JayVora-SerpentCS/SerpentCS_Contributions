cd360763a23be2e3b69e1ba2f553d235934490e5
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-05-29 16:34:12 +0530

    [MIG] ipushp: Module migrated in v13.

---
+++ ipushp/__manifest__.py
@@ -0,0 +1,25 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'iPushp',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'category': 'Human Resource',
+    'website': "http://www.serpentcs.com",
+    'summary': 'iPushp',
+    'version': '13.0.1.0.0',
+    'description': "",
+    'depends': ['hr', 'website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/website_data.xml',
+        'data/relation_data.xml',
+        'views/assets.xml',
+        'views/ipushp_config_view.xml',
+        'views/hr_employee_view.xml',
+        'views/website_ipushp_template.xml',
+        'views/ipushp_search.xml',
+        'views/find_contacts.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
