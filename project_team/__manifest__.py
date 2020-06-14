235282c6a18ed84f7827a456f1271c37977ff098
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-14 14:05:32 +0530

    Update __manifest__.py

--- project_team/__manifest__.py
+++ project_team/__manifest__.py
@@ -7,8 +7,8 @@
     'summary': 'Project Team Management',
     'category': 'Project Management',
     'website': 'http://www.serpentcs.com',
-    'version': '11.0.1.0.0',
-    'license': 'LGPL-3',
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
     'depends': [
         'project',
         'crm',
