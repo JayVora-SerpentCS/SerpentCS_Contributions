d22fd138d033ce9558f340b9a439bb66f7983d4d
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-14 14:00:29 +0530

    Update __manifest__.py

--- city/__manifest__.py
+++ city/__manifest__.py
@@ -2,12 +2,12 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "City-Helps to keep Homogeneous address data in the Database",
+    "name": "City - Keep Homogeneous address data in the Database",
     "version": "12.0.1.0.0",
     "author": "Pablo Rocandio, Serpent Consulting Services Pvt. Ltd.",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
     "category": "Hidden",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "http://www.serpentcs.com",
     "summary": "City-Helps to keep Homogeneous address data in the Database",
     "depends": ["sales_team"],
