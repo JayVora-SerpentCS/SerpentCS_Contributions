1610879154e77d56e8841c1d7ee1557a31aa195e
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-14 14:03:58 +0530

    Update __manifest__.py

--- partner_credit_limit/__manifest__.py
+++ partner_credit_limit/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Partner Credit Limit',
     'version': '12.0.1.0.0',
     'category': 'Partner',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'author': 'Tiny, Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
