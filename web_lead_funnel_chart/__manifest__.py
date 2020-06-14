48df940f359a72a12f9e41a94f66ef549dab6b50
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-14 14:09:24 +0530

    Update __manifest__.py

--- web_lead_funnel_chart/__manifest__.py
+++ web_lead_funnel_chart/__manifest__.py
@@ -1,14 +1,14 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Web Lead Funnel Chart 12.0',
+    'name': 'Web Lead Funnel Chart',
     'category': 'Web',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'summary': 'Funnel Chart for Leads & Opportunities',
     'website': 'http://www.serpentcs.com',
     'version': '12.0.1.0.1',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'depends': [
         'crm', 'sale'
     ],
