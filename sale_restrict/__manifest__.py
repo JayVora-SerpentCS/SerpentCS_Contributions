66128a28ab1f6ec2072d20455f4696f4d4db7149
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-14 14:07:02 +0530

    Update __manifest__.py

--- sale_restrict/__manifest__.py
+++ sale_restrict/__manifest__.py
@@ -1,12 +1,12 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'SO - Product Price Check',
+    'name': 'Sale Order - Product Price Check',
     'version': '12.0.1.0.0',
     'category': 'Sales Management',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'summary': 'Restrict sales on 0 value.',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'http://www.serpentcs.com',
     'depends': [
