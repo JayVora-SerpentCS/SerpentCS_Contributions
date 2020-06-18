6991b38bb8e3369d71b5f04a3f5a9365f7a08f48
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-18 14:03:10 +0530

    [12.0][IMP]Improved code as per pep8, flake8 format.

--- web_groupby_expand/static/src/js/web_groups_expand.js
+++ web_groupby_expand/static/src/js/web_groups_expand.js
@@ -1,27 +1,27 @@
-odoo.define('web_groupby_expand.web_groupby_expand', function(require) {
-"use strict";
+odoo.define('web_groupby_expand.web_groupby_expand', function (require) {
+    "use strict";
 
-var AbstractController = require('web.AbstractController');
-var core = require('web.core');
-var ListRenderer = require('web.ListRenderer')
-var config = require('web.config');
+    var AbstractController = require('web.AbstractController');
+    var core = require('web.core');
+    var ListRenderer = require('web.ListRenderer');
+    var config = require('web.config');
 
-var QWeb = core.qweb;
+    var QWeb = core.qweb;
 
-ListRenderer.include({
-    init: function (parent, state, params) {
-        var self = this;
-        var res = this._super.apply(this, arguments);
-        this.expand = false;
-        this.in_expand = false;
-        return res;
-    },
-    render_auto_groups: function() {
-        var self = this;
-        var is_grouped = !!self.state.groupedBy.length;
+    ListRenderer.include({
+        init: function (parent, state, params) {
+            var self = this;
+            var res = this._super.apply(this, arguments);
+            this.expand = false;
+            this.in_expand = false;
+            return res;
+        },
+        render_auto_groups: function () {
+            var self = this;
+            var is_grouped = !!self.state.groupedBy.length;
         if (is_grouped) {
             self.expand = true;
-            _.each(self.state.data, function(group) {
+            _.each(self.state.data, function (group) {
                 if(group){
                     self.trigger_up('toggle_group', {group: group});
                 }
@@ -50,7 +50,8 @@ AbstractController.include({
     _renderSwitchButtons: function () {
         var self = this
         var res = this._super.apply(this, arguments);
-        var totalViews = _.filter(this.actionViews, {multiRecord: this.isMultiRecord});
+        var totalViews = _.filter(this.actionViews,
+            {multiRecord: this.isMultiRecord});
         if (totalViews.length <= 1 ) {
             var template = 'ControlPanel.SingleViewSwitchButtons';
             res = $(QWeb.render(template, {
@@ -58,7 +59,7 @@ AbstractController.include({
             }));
         }
         var oe_list_expand;
-        _.each(res, function(rec){
+        _.each(res, function (rec){
             if (rec.id == 'expand_icon'){
                 oe_list_expand = $(rec);
             }
@@ -74,41 +75,41 @@ AbstractController.include({
             } else {
                 oe_list_expand.hide();
             }
-            oe_list_expand.unbind('click').bind('click', function() {
-                var is_grouped = !!self.renderer.state.groupedBy.length;
-                if (is_grouped) {
-                    if ($(this).hasClass('fa-expand')){
-                        self.renderer.in_expand = true;
-                        $(this).removeClass('fa-expand');
-                        $(this).addClass('fa-compress');
-                    }else{
-                        self.renderer.in_expand = false;
-                        $(this).addClass('fa-expand');
-                        $(this).removeClass('fa-compress');
+            oe_list_expand.unbind('click').bind('click', function () {
+                    var is_grouped = !!self.renderer.state.groupedBy.length;
+                    if (is_grouped) {
+                        if ($(this).hasClass('fa-expand')) {
+                            self.renderer.in_expand = true;
+                            $(this).removeClass('fa-expand');
+                            $(this).addClass('fa-compress');
+                        } else {
+                            self.renderer.in_expand = false;
+                            $(this).addClass('fa-expand');
+                            $(this).removeClass('fa-compress');
+                        }
+                        self.renderer.render_auto_groups();
                     }
-                    self.renderer.render_auto_groups();
+                });
+            }
+            return res;
+        },
+        update: function (params, options) {
+            var self = this;
+            return this._super.apply(this, arguments).then(function () {
+                if (self.viewType === 'list') {
+                    $('#expand_icon').show();
+                    if (self.renderer.state.groupedBy) {
+                        if (self.renderer.state.groupedBy.length > 0) {
+                            $('#expand_icon').show();
+                        } else if (self.renderer.state.groupedBy.length ===0) {
+                            $('#expand_icon').hide();
+                        }
+                    }
+                } else {
+                    $('#expand_icon').hide();
                 }
             });
-        }
-        return res;
-    },
-    update: function (params, options) {
-        var self = this
-        return this._super.apply(this, arguments).then(function(){
-            if(self.viewType === 'list'){
-                $('#expand_icon').show()
-                if(self.renderer.state.groupedBy){
-                    if(self.renderer.state.groupedBy.length > 0){
-                        $('#expand_icon').show()
-                    } else if(self.renderer.state.groupedBy.length == 0){
-                        $('#expand_icon').hide()
-                    }
-                }
-            } else {
-                $('#expand_icon').hide()
-            }
-        });
-    }
-});
+        },
+    });
 
 });
