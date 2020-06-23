208c0a0eac4aa33483d377e0190c30ad40c42e01
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-23 17:47:33 +0530

    [PATCH 42/46] [13.0][IMP] label : Fixed the travis issue.

--- label/models/label_config.py
+++ label/models/label_config.py
@@ -4,20 +4,19 @@ from odoo import models, fields
 
 
 class LabelMain(models.Model):
-    _name = 'label.brand'
-    _rec_name = 'brand_name'
+    _name = "label.brand"
+    _rec_name = "brand_name"
 
-    _description = 'Configured Label Brands'
+    _description = "Configured Label Brands"
 
-    brand_name = fields.Char('Name', size=64, index=True)
-    label_config_ids = fields.One2many('label.config', 'label_main_id',
-                                       'Label Config')
+    brand_name = fields.Char("Name", size=64, index=True)
+    label_config_ids = fields.One2many("label.config", "label_main_id", "Label Config")
 
 
 class LabelConfig(models.Model):
-    _name = 'label.config'
+    _name = "label.config"
 
-    _description = 'Configuration for label'
+    _description = "Configuration for label"
 
     name = fields.Char("Name", size=64, required=True, index=True)
     height = fields.Float("Height (in mm)", required=True)
@@ -27,4 +26,4 @@ class LabelConfig(models.Model):
     left_margin = fields.Float("Left Margin (in mm)", default=0.0)
     right_margin = fields.Float("Right Margin (in mm)", default=0.0)
     cell_spacing = fields.Float("Cell Spacing", default=1.0)
-    label_main_id = fields.Many2one('label.brand', 'Label')
+    label_main_id = fields.Many2one("label.brand", "Label")
