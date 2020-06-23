574882ae0028560371bc428a711252a79d2243aa
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-23 17:20:26 +0530

    hr_grade_rank : Fixed the travis issue.

--- hr_grade_rank/models/hr_grade_rank.py
+++ hr_grade_rank/models/hr_grade_rank.py
@@ -4,35 +4,33 @@ from odoo import fields, models, api
 
 
 class RankRank(models.Model):
-    _name = 'rank.rank'
+    _name = "rank.rank"
 
-    name = fields.Char('Name')
-    description = fields.Text('Description')
-    salary_range = fields.Text('Salary Range')
-    grade_id = fields.Many2one('grade.grade', 'Grade')
+    name = fields.Char("Name")
+    description = fields.Text("Description")
+    salary_range = fields.Text("Salary Range")
+    grade_id = fields.Many2one("grade.grade", "Grade")
 
 
 class GradeGrade(models.Model):
-    _name = 'grade.grade'
+    _name = "grade.grade"
 
-    name = fields.Char('Name')
-    description = fields.Text('Description')
-    rank_ids = fields.One2many('rank.rank', 'grade_id', 'Ranks')
+    name = fields.Char("Name")
+    description = fields.Text("Description")
+    rank_ids = fields.One2many("rank.rank", "grade_id", "Ranks")
 
 
 class HrEmployee(models.Model):
 
-    _inherit = 'hr.employee'
+    _inherit = "hr.employee"
 
-    grade_id = fields.Many2one('grade.grade', 'Grade')
-    rank_id = fields.Many2one('rank.rank', 'Rank')
+    grade_id = fields.Many2one("grade.grade", "Grade")
+    rank_id = fields.Many2one("rank.rank", "Rank")
 
-    @api.onchange('grade_id')
+    @api.onchange("grade_id")
     def onchange_grade(self):
         res = {}
         if self.grade_id:
             self.rank_id = False
-            res['domain'] = \
-                {'rank_id': [
-                    ('id', 'in', self.grade_id.rank_ids.ids)]}
+            res["domain"] = {"rank_id": [("id", "in", self.grade_id.rank_ids.ids)]}
         return res
