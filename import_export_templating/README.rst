fc90c3210646d0ba807b84234142a55b7b53f2bd
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-23 17:30:36 +0530

    [13.0][IMP] import_export_templating : Fixed the travis issue.

---
+++ import_export_templating/README.rst
@@ -0,0 +1,20 @@
+==========================
+Import / Export Templating
+==========================
+
+* This module allows you to Export / Import any data of model.
+* You just need to Download Blank Template based on type selection and To Import select type and load downloded file with data.
+
+Usage
+=====
+
+Bug Tracker
+===========
+
+Credits
+=======
+
+Contributors
+------------
+
+* Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
--- import_export_templating/__manifest__.py
+++ import_export_templating/__manifest__.py
@@ -1,24 +1,21 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Import / Export Templating',
-    'version': '13.0.1.0.0',
-    'license': 'LGPL-3',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-    'website': 'http://www.serpentcs.com',
-    'category': 'Base',
-    'summary': """
+    "name": "Import / Export Templating",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "category": "Base",
+    "summary": """
     Standard import export template,
     User-friendly templates to upload download bulk data Manage easy
     import-export for non-technical users of ERP.
     """,
-    'depends': ['web'],
-    'data': [
-        'wizard/wiz_download_template_view.xml',
-        'views/templates.xml',
-    ],
-    'installable': True,
-    'price': 30.0,
-    'currency': 'EUR',
+    "depends": ["web"],
+    "data": ["wizard/wiz_download_template_view.xml", "views/templates.xml"],
+    "installable": True,
+    "price": 30.0,
+    "currency": "EUR",
 }
