64f5b368f9747949696a210494d02b8d64cf80e8
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-24 17:01:27 +0530

    [13.0][IMP] sale_report_hide_price : Fixed the travis issue.

--- sale_report_hide_price/tests/test_sale.py
+++ sale_report_hide_price/tests/test_sale.py
@@ -4,25 +4,35 @@ from odoo.tests import common
 
 
 class SaleOrderTest(common.TransactionCase):
-
     def setup(self):
-        super(SaleOrderTest, self).setup()
+        return super(SaleOrderTest, self).setup()
 
     def test_saleorder_action(self):
-        self.sale_order = self.env['sale.order']
-        self.partner = self.env.ref('base.res_partner_2')
+        self.sale_order = self.env["sale.order"]
+        self.partner = self.env.ref("base.res_partner_2")
         self.products = {
-            'serv_order': self.env.ref('product.service_order_01'),
-            'serv_deli': self.env.ref('product.service_delivery'),
+            "serv_order": self.env.ref("product.service_order_01"),
+            "serv_deli": self.env.ref("product.service_delivery"),
         }
 
-        self.order_idA = self.sale_order.create({
-            'partner_id': self.partner.id,
-            'show_price': True,
-            'show_discount': True,
-            'order_line': [
-                (0, 0, {'name': p.name, 'product_id': p.id,
-                        'product_uom_qty': 2, 'product_uom': p.uom_id.id,
-                        'price_unit': p.list_price
-                        })for (_, p) in self.products.items()],
-        })
+        self.order_idA = self.sale_order.create(
+            {
+                "partner_id": self.partner.id,
+                "show_price": True,
+                "show_discount": True,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": p.name,
+                            "product_id": p.id,
+                            "product_uom_qty": 2,
+                            "product_uom": p.uom_id.id,
+                            "price_unit": p.list_price,
+                        },
+                    )
+                    for (_, p) in self.products.items()
+                ],
+            }
+        )
