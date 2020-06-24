5df69da348868d2f4f597af54bb29a24607e104d
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-24 14:36:52 +0530

    project_detail_report : Fixed the travis issue.

--- project_detail_report/__manifest__.py
+++ project_detail_report/__manifest__.py
@@ -1,21 +1,16 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Project Report',
-    'version': '13.0.1.0.0',
-    'category': 'Project Management',
-    'license': 'AGPL-3',
-    'summary': """Print Project Detail report with task
+    "name": "Project Report",
+    "version": "13.0.1.0.0",
+    "category": "Project Management",
+    "license": "AGPL-3",
+    "summary": """Print Project Detail report with task
                     list and task details.""",
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'website': 'http://www.serpentcs.com',
-    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-    'depends': [
-        'hr_timesheet',
-    ],
-    'data': [
-        'views/project_report.xml',
-        'report/project_qweb_report.xml',
-    ],
-    'installable': True,
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "depends": ["hr_timesheet"],
+    "data": ["views/project_report.xml", "report/project_qweb_report.xml"],
+    "installable": True,
 }
