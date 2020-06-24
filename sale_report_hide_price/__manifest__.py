4641e980898ec3735109e5ab95b926794e7b156a
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-24 17:01:27 +0530

    [13.0][IMP] sale_report_hide_price : Fixed the travis issue.

--- sale_report_hide_price/__manifest__.py
+++ sale_report_hide_price/__manifest__.py
@@ -1,22 +1,20 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Hide Price and Discount in Quotation Report',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-    'category': 'Sales Management',
-    'website': 'http://www.serpentcs.com',
-    'version': '13.0.1.0.1',
-    'license': 'AGPL-3',
-    'summary': 'Hide price/discount in Sale Order Report',
-    'depends': [
-        'sale'
+    "name": "Hide Price and Discount in Quotation Report",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "category": "Sales Management",
+    "website": "http://www.serpentcs.com",
+    "version": "13.0.1.0.1",
+    "license": "AGPL-3",
+    "summary": "Hide price/discount in Sale Order Report",
+    "depends": ["sale"],
+    "images": ["static/description/icon.png"],
+    "data": [
+        "views/sale_view.xml",
+        "views/report_saleorder.xml",
+        "views/preview_sale_order.xml",
     ],
-    'images': ['static/description/icon.png'],
-    'data': [
-        'views/sale_view.xml',
-        'views/report_saleorder.xml',
-        'views/preview_sale_order.xml'
-    ],
-    'installable': True,
+    "installable": True,
 }
