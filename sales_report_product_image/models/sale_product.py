192232ca5d5c5a7afb7bc9ae0dd3484a955257f2
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-25 18:23:40 +0530

    [13.0][IMP] sales_report_product_image : Fixed the travis issue.

--- sales_report_product_image/models/sale_product.py
+++ sales_report_product_image/models/sale_product.py
@@ -4,20 +4,26 @@ from odoo import fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     print_image = fields.Boolean(
-        'Print Image', help="""If ticked, you can see the product image in
-        report of sale order/quotation""")
+        "Print Image",
+        help="""If ticked, you can see the product image in
+        report of sale order/quotation""",
+    )
     image_sizes = fields.Selection(
-        [('image', 'Big sized Image'), ('image_medium', 'Medium Sized Image'),
-         ('image_small', 'Small Sized Image')],
-        'Image Sizes', default="image_small",
-        help="Image size to be displayed in report")
+        [
+            ("image", "Big sized Image"),
+            ("image_medium", "Medium Sized Image"),
+            ("image_small", "Small Sized Image"),
+        ],
+        "Image Sizes",
+        default="image_small",
+        help="Image size to be displayed in report",
+    )
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    image_small = fields.Binary(
-        'Product Image', related='product_id.image_1920')
+    image_small = fields.Binary("Product Image", related="product_id.image_1920")
