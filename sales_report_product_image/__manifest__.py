c22f34f250a7c16c59caa8a8f1eb938339884b13
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-25 18:23:40 +0530

    [13.0][IMP] sales_report_product_image : Fixed the travis issue.

--- sales_report_product_image/__manifest__.py
+++ sales_report_product_image/__manifest__.py
@@ -2,31 +2,22 @@
 
 {
     # Module information
-    'name': 'Product Image for Sale',
-    'version': '13.0.1.0.0',
-    'category': 'Sales Management',
-    'sequence': '1',
-    'summary': """Product Image for Quatation/Sale Reports.""",
-    'description': """Product Image for Quatation/Sale Reports.""",
-    'license': 'LGPL-3',
-
+    "name": "Product Image for Sale",
+    "version": "13.0.1.0.0",
+    "category": "Sales Management",
+    "sequence": "1",
+    "summary": """Product Image for Quatation/Sale Reports.""",
+    "description": """Product Image for Quatation/Sale Reports.""",
+    "license": "LGPL-3",
     # Author
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'website': 'http://www.serpentcs.com',
-    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
     # Dependencies
-    'depends': [
-        'sale_management',
-    ],
-
+    "depends": ["sale_management"],
     # Views
-    'data': [
-        'views/sale_product_view.xml',
-        'views/report_saleorder.xml',
-    ],
-
+    "data": ["views/sale_product_view.xml", "views/report_saleorder.xml"],
     # Techical
-    'installable': True,
-    'auto_install': False,
+    "installable": True,
+    "auto_install": False,
 }
