ac40c1f2c290536dcadd4609e5b72832137bf1fa
RiddhiChavda22 <riddhi.odoo@gmail.com>
2020-06-25 18:12:45 +0530

    [13.0][IMP] website_product_brand : Fixed the travis issue.

--- website_product_brand/__manifest__.py
+++ website_product_brand/__manifest__.py
@@ -1,19 +1,16 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Product Brand Filtering in Website',
-    'category': 'Website',
-    'author': 'Serpent Consulting Services Pvt. Ltd.',
-    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-    'website': 'https://www.serpentcs.com',
-    'license': 'AGPL-3',
-    'summary': 'Product Brand based filters',
-    'version': '13.0.0.1.0',
-    'depends': ['product_brand', 'website_sale'],
-    'data': [
-        "security/ir.model.access.csv",
-        "views/product_brand.xml",
-    ],
-    'installable': True,
-    'auto_install': False,
+    "name": "Product Brand Filtering in Website",
+    "category": "Website",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "https://www.serpentcs.com",
+    "license": "AGPL-3",
+    "summary": "Product Brand based filters",
+    "version": "13.0.0.1.0",
+    "depends": ["product_brand", "website_sale"],
+    "data": ["security/ir.model.access.csv", "views/product_brand.xml"],
+    "installable": True,
+    "auto_install": False,
 }
