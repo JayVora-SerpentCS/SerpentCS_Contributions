5ff66c75ccd9e149cecc9787753fa2f22992af2e
Pratik Vaza <pvstranger00@gmail.com>
2020-09-23 15:18:26 +0530

    [IMP]:Improved the fields names

--- project_team/models/crm_team.py
+++ project_team/models/crm_team.py
@@ -8,8 +8,8 @@ class CrmTeamInherit(models.Model):
 
     type_team = fields.Selection([('sale', 'Sale'), ('project', 'Project')],
                                  "Type", default="sale")
-    team_member_ids = fields.Many2many('res.users', 'project_team_user_rel',
-                                       'team_id', 'uid', 'Project Members',
-                                       help="""Project's members are users who
+    team_members_ids = fields.Many2many('res.users', 'project_team_user_rel',
+                                        'team_id', 'user_id', 'Project Members',
+                                        help="""Project's members are users who
                                      can have an access to the tasks related
                                      to this project.""")
