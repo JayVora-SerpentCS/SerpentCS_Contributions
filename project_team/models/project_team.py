2a4d5741bbded4a57dc3e2005a03ce8fdb1afddf
Pratik Vaza <pvstranger00@gmail.com>
2020-09-23 13:25:29 +0530

    project_team:migrated the module and improved the code and field names

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -6,17 +6,15 @@ from odoo import api, fields, models
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    members = fields.Many2many('res.users', 'project_user_rel', 'project_id',
-                               'uid', 'Project Members', help="""Project's
+    member_ids = fields.Many2many('res.users', 'project_user_rel', 'project_id',
+                                  'uid', 'Project Members', help="""Project's
                                members are users who can have an access to
                                the tasks related to this project."""
-                               )
+                                  )
     team_id = fields.Many2one('crm.team', "Project Team",
                               domain=[('type_team', '=', 'project')])
 
     @api.onchange('team_id')
-    def get_team_members(self):
-        self.members = False
-        if self.team_id:
-            self.members = \
-                [(6, 0, [member.id for member in self.team_id.team_members])]
+    def _get_team_members(self):
+        self.update({"member_ids": [(6, 0,
+                                    self.team_id.team_member_ids.ids)]})
