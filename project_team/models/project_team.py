d444dfee9a542e210c22f32376383b5e6da6b515
Pratik Vaza <pvstranger00@gmail.com>
2020-09-23 15:18:26 +0530

    [IMP]:Improved the fields names

--- project_team/models/project_team.py
+++ project_team/models/project_team.py
@@ -6,15 +6,15 @@ from odoo import api, fields, models
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    member_ids = fields.Many2many('res.users', 'project_user_rel', 'project_id',
-                                  'uid', 'Project Members', help="""Project's
+    members_ids = fields.Many2many('res.users', 'project_user_rel', 'project_id',
+                                   'user_id', 'Project Members', help="""Project's
                                members are users who can have an access to
                                the tasks related to this project."""
-                                  )
+                                   )
     team_id = fields.Many2one('crm.team', "Project Team",
                               domain=[('type_team', '=', 'project')])
 
     @api.onchange('team_id')
     def _get_team_members(self):
-        self.update({"member_ids": [(6, 0,
-                                    self.team_id.team_member_ids.ids)]})
+        self.update({"members_ids": [(6, 0,
+                                     self.team_id.team_members_ids.ids)]})
