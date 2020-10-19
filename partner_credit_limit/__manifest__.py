7fd5792a3a8518e7e5def471305861d4585e38a0
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-10-19 23:58:52 +0530

    Migrated the module in v14.

--- partner_credit_limit/__manifest__.py
+++ partner_credit_limit/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Partner Credit Limit',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Partner',
     'license': 'AGPL-3',
     'author': 'Tiny, Serpent Consulting Services Pvt. Ltd.',
