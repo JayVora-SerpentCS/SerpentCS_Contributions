5419264246958fc50ad9122d8bcc0d93003f0d12
Foram Darji <foram.d.serpentcs@gmail.com>
2020-10-28 17:36:23 +0530

    base_synchro: Migrated module in V14.

---
+++ base_synchro/__manifest__.py
@@ -0,0 +1,21 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Multi-DB Synchronization",
+    "version": "14.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Multi-DB Synchronization",
+    "author": "OpenERP SA, Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "images": ["static/description/Synchro.png"],
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/base_synchro_view.xml",
+        "views/base_synchro_view.xml",
+        "views/res_request_view.xml",
+    ],
+    "installable": True,
+}
---
+++ base_synchro/models/__init__.py
@@ -0,0 +1,4 @@
+# See LICENSE file for full copyright and licensing details.
+
+from . import base_synchro_obj
+from . import res_request
