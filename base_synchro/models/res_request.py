cb1fbd79e1f788f7547eb3f86cac67675304cc97
Foram Darji <foram.d.serpentcs@gmail.com>
2020-10-28 17:36:23 +0530

    [14.0][MIG]base_synchro: Migrated module in V14.

---
+++ base_synchro/models/res_request.py
@@ -0,0 +1,15 @@
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResRequest(models.Model):
+    _name = "res.request"
+    _order = "date desc"
+    _description = "Request"
+
+    name = fields.Char("Subject", required=True)
+    date = fields.Datetime("Date")
+    act_from = fields.Many2one("res.users", "From")
+    act_to = fields.Many2one("res.users", "To")
+    body = fields.Text("Request")
