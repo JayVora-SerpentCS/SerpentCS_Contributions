0fccc9ab93e6c865da6311e05535f1b6616bf4a1
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-11-10 17:27:42 +0530

    Migrated the module in v14.

--- sale_report_hide_price/README.rst
+++ sale_report_hide_price/README.rst
@@ -1,7 +1,3 @@
-.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-   :target: https://www.gnu.org/licenses/agpl
-   :alt: License: AGPL-3
-
 ======================
 Sale Report Hide Price
 ======================
@@ -25,20 +21,4 @@ Credits
 Contributors
 ------------
 
-* Jay Vora <jay.vora@serpentcs.com>
-
-Maintainer
-----------
-
-.. image:: http://odoo-community.org/logo.png
-   :alt: Odoo Community Association
-   :target: http://odoo-community.org
-
-This module is maintained by the OCA.
-
-OCA, or the Odoo Community Association, is a nonprofit organization whose
-mission is to support the collaborative development of Odoo features and
-promote its widespread use.
-
-To contribute to this module, please visit http://odoo-community.org.
 * Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
--- sale_report_hide_price/tests/test_sale.py
+++ sale_report_hide_price/tests/test_sale.py
@@ -10,9 +10,33 @@ class SaleOrderTest(common.TransactionCase):
     def test_saleorder_action(self):
         self.sale_order = self.env["sale.order"]
         self.partner = self.env.ref("base.res_partner_2")
+        service_delivery = self.env['product.product'].create({
+            'name': 'Cost-plus Contract',
+            'categ_id': self.env.ref('product.product_category_5').id,
+            'standard_price': 200.0,
+            'list_price': 180.0,
+            'type': 'service',
+            'uom_id': self.env.ref('uom.product_uom_unit').id,
+            'uom_po_id': self.env.ref('uom.product_uom_unit').id,
+            'default_code': 'SERV_DEL',
+            'invoice_policy': 'delivery',
+        })
+        service_order_01 = self.env['product.product'].create({
+            'name': 'Remodeling Service',
+            'categ_id': self.env.ref('product.product_category_3').id,
+            'standard_price': 40.0,
+            'list_price': 90.0,
+            'type': 'service',
+            'uom_id': self.env.ref('uom.product_uom_hour').id,
+            'uom_po_id': self.env.ref('uom.product_uom_hour').id,
+            'description': 'Example of product to invoice on order',
+            'default_code': 'PRE-PAID',
+            'invoice_policy': 'order',
+        })
+
         self.products = {
-            "serv_order": self.env.ref("product.service_order_01"),
-            "serv_deli": self.env.ref("product.service_delivery"),
+            "serv_order": service_order_01,
+            "serv_deli": service_delivery,
         }
 
         self.order_idA = self.sale_order.create(
