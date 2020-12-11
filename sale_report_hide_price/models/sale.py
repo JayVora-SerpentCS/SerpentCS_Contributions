b7ec7bb49c8c8b5e73199113646bd3d679c68a99
Pragati Patel <pragatipp98@gmail.com>
2020-12-11 12:30:12 +0530

    [IMP] sale_report_hide_price : improve the code

--- sale_report_hide_price/models/sale.py
+++ sale_report_hide_price/models/sale.py
@@ -7,15 +7,15 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     show_price = fields.Boolean(
-        groups="account.group_account_manager"
         "Show Price",
+        groups="account.group_account_manager",
         help="If checked, you can see the price in "
         "report of"
         "Sales Order / Quotation.",
     )
     show_discount = fields.Boolean(
-        groups="account.group_account_manager"
         "Show Discount",
+        groups="account.group_account_manager",
         help="If checked, you can see the discount"
         " in report of Sales Order/Quotation.",
     )
