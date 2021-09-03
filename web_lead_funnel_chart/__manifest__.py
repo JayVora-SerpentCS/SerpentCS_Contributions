0faab6db2e22aa1e29ebbee2d4c240fbec7c68f3
Drashti Patel - SerpentCS <d.patel.serpentcs@gmail.com>
2021-09-03 14:39:38 +0530

    web_lead_funnel_chart: Migrated module.

---
+++ web_lead_funnel_chart/__manifest__.py
@@ -0,0 +1,17 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Web Lead Funnel Chart 14.0",
+    "category": "Web",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "summary": "Funnel Chart for Leads & Opportunities",
+    "website": "https://serpentcs.com/",
+    "version": "14.0.1.0.1",
+    'license': "LGPL-3",
+    "depends": ["crm", "sale"],
+    "data": ["views/templates.xml", "views/web_lead_funnel_chart_view.xml"],
+    "images": ["static/description/FunnelChart.png"],
+    "installable": True,
+    "auto_install": False,
+}
