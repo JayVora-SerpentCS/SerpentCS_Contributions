a99f6255f4752cededed9f85ce71aa5dbce22b31
ilakkia-serpentcs <ila.d.serpentcs@gmail.com>
2022-01-11 13:09:16 +0400

    [15.0][Mig]hr_applicant

---
+++ hr_applicant/__manifest__.py
@@ -0,0 +1,25 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Applicant Process",
+    "version": "15.0.0.1.0",
+    "category": "Human Resources",
+    "sequence": 90,
+    "license": "AGPL-3",
+    "summary": "Applicant and Employee Subsections, Training",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "website": "http://www.serpentcs.com",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "depends": ["hr_recruitment"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/select_training_view.xml",
+        "report/report_view.xml",
+        "views/hr_recruitment_views.xml",
+        "views/hr_recruitment_employee_views.xml",
+        "views/training_views.xml",
+        "views/applicant_profile_report_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ hr_applicant/models/__init__.py
@@ -0,0 +1,5 @@
+# See LICENSE file for full copyright and licensing details.
+
+from . import hr_recruitment
+from . import hr_recruitment_employee
+from . import training
