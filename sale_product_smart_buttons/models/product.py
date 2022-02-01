a9588c22e7b8e4c048a39cdf02f0749403770800
ilakkia-serpentcs <ila.d.serpentcs@gmail.com>
2022-02-01 16:29:14 +0400

    [15.0][MIG]sale_product_smart_buttons

--- sale_product_smart_buttons/models/product.py
+++ sale_product_smart_buttons/models/product.py
@@ -4,36 +4,44 @@ from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = "product.template"
+    _inherit = 'product.template'
 
-    @api.depends("product_variant_ids.sales_amt")
+   
+    @api.depends('product_variant_ids.sales_amt')
     def _compute_sales_amt(self):
         for product in self:
-            product.sales_amt = sum([p.sales_amt for p in product.product_variant_ids])
 
-    sales_amt = fields.Float(compute="_compute_sales_amt", string="Sales amt")
+            product.sales_amt = sum([p.sales_amt
+                                     for p in product.product_variant_ids])
+           
+
+    sales_amt = fields.Float(compute='_compute_sales_amt', string='Sales amt')
 
 
 class ProductProduct(models.Model):
-    _inherit = "product.product"
+    _inherit = 'product.product'
 
+    @api.depends('product_tmpl_id.sales_amt')
     def _compute_sales_amt(self):
         res = {}
         amt = 0.00
-        sales_amount = 0.0
-        order_lines = self.env["sale.order.line"].search([
-            ("order_id.state", "in", ["sale", "done"]),
-            ("product_id", "in", self.ids),
-        ])
+        domain = [
+            ('order_id.state', 'in', ['sale', 'done']),
+            ('product_id', 'in', self.ids),
+        ]
 
+        order_lines = self.env['sale.order.line'].search(domain)
         for line in order_lines:
-            amt += line.price_subtotal
-            res[line.product_id.id] = {"amt": amt}
+            if res.get(line.product_id.id):
+               res.get(line.product_id.id).update({'amt':res.get(line.product_id.id).get('amt')+line.price_subtotal})
+            else:
+                res[line.product_id.id] = {'amt':line.price_subtotal}
+           
         for product in self:
-            if res:
-                sales_amount = (
-                    res[product.id].get("amt", 0.0) if product.id in res else 0.0
-                )
-        self.sales_amt = sales_amount
+            product.sales_amt = 0.0 
+            if res and res.get(product.id):
+                product.sales_amt=res.get(product.id).get('amt',0)
 
-    sales_amt = fields.Float(compute="_compute_sales_amt", string="# Sales amt")
+
+
+    sales_amt = fields.Float(compute='_compute_sales_amt', string='# Sales amt')
