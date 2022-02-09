a7e21d779faba457fea149ff2b9226809dd48b1b
ilakkia-serpentcs <ila.d.serpentcs@gmail.com>
2022-02-09 15:35:09 +0400

    [15.0][MIG]updated  module as per the comments

--- sale_report_hide_price/__init__.py
+++ sale_report_hide_price/__init__.py
@@ -1,4 +1,4 @@
 # © 2016 Serpent Consulting Services Pvt. Ltd. <sebastien.beau@akretion.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
--- sale_report_hide_price/tests/test_sale.py
+++ sale_report_hide_price/tests/test_sale.py
@@ -9,20 +9,21 @@ class SaleOrderTest(common.TransactionCase):
         super(SaleOrderTest, self).setup()
 
     def test_saleorder_action(self):
-        self.sale_order = self.env['sale.order']
+        self.sale_order_obj = self.env['sale.order']
         self.partner = self.env.ref('base.res_partner_2')
         self.products = {
             'serv_order': self.env.ref('product.product_delivery_01'),
             'serv_deli': self.env.ref('product.product_order_01'),
         }
 
-        self.order_idA = self.sale_order.create({
+        self.order_idA = self.sale_order_obj.create({
             'partner_id': self.partner.id,
             'show_price': True,
             'show_discount': True,
             'order_line': [
-                (0, 0, {'name': p.name, 'product_id': p.id,
-                        'product_uom_qty': 2, 'product_uom': p.uom_id.id,
-                        'price_unit': p.list_price
-                        }) for (_, p) in self.products.items()],
+                (0, 0, {'name': product_1.name, 'product_id': product_1.id,
+                        'product_uom_qty': 2, 'product_uom': product_1.uom_id.id,
+                        'price_unit': product_1.list_price
+                        }) for (_, product_1) in self.products.items()],
+
         })
