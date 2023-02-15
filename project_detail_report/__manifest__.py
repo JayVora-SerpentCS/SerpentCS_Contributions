2f27bcdbac9c7ca9e225afe03e06fb6922690653
PratikVaza-SerpentCS <pratik.v.serpentcs@gmail.com>
2023-02-15 12:56:56 +0530

    [MIG][16.0] project_detail_report: Migrated the module.

--- project_detail_report/__manifest__.py
+++ project_detail_report/__manifest__.py
@@ -2,13 +2,13 @@
 
 {
     "name": "Project Report",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Project Management",
     "license": "AGPL-3",
     "summary": """Print Project Detail report with task
                     list and task details.""",
     "author": "Serpent Consulting Services Pvt. Ltd.",
-    "website": "http://www.serpentcs.com",
+    "website": "https://www.serpentcs.com",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
     "depends": ["hr_timesheet"],
     "data": ["views/project_report.xml", "report/project_qweb_report.xml"],
