79e29e45ac1c57a15d43c07a19fc867eed28f018
PratikVaza-SerpentCS <pratik.v.serpentcs@gmail.com>
2023-02-15 11:11:25 +0530

    hr_grade_rank: Migrated the module.

--- hr_grade_rank/__manifest__.py
+++ hr_grade_rank/__manifest__.py
@@ -2,13 +2,13 @@
 
 {
     "name": "HR-Grade Rank",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Manage grade and rank of employee",
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
     "category": "Human Resources",
-    "website": "http://www.serpentcs.com",
+    "website": "https://www.serpentcs.com",
     "depends": ["hr"],
     "data": ["security/ir.model.access.csv", "views/hr_grade_rank_view.xml"],
     "images": ["static/description/HRGradeRank.png"],
--- hr_grade_rank/models/hr_grade_rank.py
+++ hr_grade_rank/models/hr_grade_rank.py
@@ -10,9 +10,24 @@ class RankRank(models.Model):
     name = fields.Char()
     description = fields.Text()
     salary_range = fields.Text()
-    active = fields.Boolean('Active',default=True)
+    active = fields.Boolean(default=True)
 
-    _sql_constraints=[('name_uniq', 'unique(name)',"Rank should be unique")]
+    _sql_constraints = [("name_uniq", "unique(name)", "Rank should be unique")]
+
+    @api.model
+    def name_search(self, name="", args=None, operator="ilike", limit=80):
+        """Search the rank based on grade."""
+        if self.env.context.get("grade"):
+            grade_rank_recs = (
+                self.env["grade.grade"]
+                .sudo()
+                .search([("id", "=", self.env.context.get("grade"))])
+                .mapped("rank_ids")
+            )
+            args += [("id", "in", grade_rank_recs.ids or [])]
+        return super(RankRank, self).name_search(
+            name=name, args=args, operator=operator, limit=limit
+        )
 
 
 class GradeGrade(models.Model):
@@ -21,8 +36,10 @@ class GradeGrade(models.Model):
 
     name = fields.Char()
     description = fields.Text()
-    rank_ids=fields.Many2many("rank.rank","rel_grade_rank","grade_id","rank_id","Ranks")
-    active = fields.Boolean('Active',default=True)
+    rank_ids = fields.Many2many(
+        "rank.rank", "rel_grade_rank", "grade_id", "rank_id", "Ranks"
+    )
+    active = fields.Boolean(default=True)
 
 
 class HrEmployee(models.Model):
@@ -31,13 +48,3 @@ class HrEmployee(models.Model):
 
     grade_id = fields.Many2one("grade.grade", "Grade")
     rank_id = fields.Many2one("rank.rank", "Rank")
-    
-
-    @api.onchange("grade_id")
-    def _onchange_grade(self):
-        res = {}
-        if self.grade_id:
-            self.rank_id = False
-            res["domain"] = {"rank_id": [
-                ("id", "in", self.grade_id.rank_ids.ids)]}
-        return res
