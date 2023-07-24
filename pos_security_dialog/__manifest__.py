19cf2b515f7e8c5e55cf56675f24239ac7e84b32
Parvez Qureshi <parvez.qureshi@serpentcs.com>
2023-07-24 11:51:19 +0530

    [IMP]Improved index file

--- pos_security_dialog/__manifest__.py
+++ pos_security_dialog/__manifest__.py
@@ -1,20 +1,33 @@
 # See LICENSE file for full copyright and licensing details.
 
 {
+    # Module information
     'name': 'POS Security Dialog',
     'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
     'category': 'Web',
-    'summary': 'POS Security Dialog',
-    'description': """POS Security Dialog""",
+    'summary': 'This module provides the functionality for POS security on the type of button.',
+    'description': """This module provides the functionality for POS security on the type of button.""",
+    
+    # Author
     'author': 'Serpent Consulting Services Pvt. Ltd.',
     'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
-    'license': 'AGPL-3',
     'website': 'https://www.serpentcs.com',
+    
+    # Dependancies
     'depends': ['point_of_sale', 'web_security_dialog'],
+    
+    # Views
     'data': [
         'views/pos_config_view.xml',
         'views/templates.xml'
     ],
+    
+    
+    # Odoo App Store Specific
+    'images': ['static/description/Odoo-app-pos-security-dailog.png'],
+    
+    # Technical
     'installable': True,
     'application': True
 }
