2f6c6ef354c601b47f816f3bfe61b9a92c1cdbe5
Nirmal <nirmal.s.serpentcs@gmail.com>
2024-01-08 17:50:47 +0530

    [MIG][17.0] migrated module ipushp

--- ipushp/static/src/js/custom.js
+++ ipushp/static/src/js/custom.js
@@ -15,6 +15,24 @@ $( document ).ready(function () {
         }
     }
 });
+
+$(document).on("click",".check_required_fields", function(){
+    var flag = 0;
+    var field_list = ['business_categ_id','category_name','relation_id','name','phone','email','description'];
+
+    field_list.forEach(function (e) {
+        if ($(`input[name='${e}']`).val() == '') {
+            $(`input[name='${e}']`).css({ 'border': '1px solid red' });
+            return false;
+        }
+        else{
+            $(`input[name='${e}']`).css({ 'border': '' });
+        }
+    });
+     
+
+});
+
 $(document).on("change", ".select_business_categ", function () {
     if ($(this).val() == -1) {
         $(".form_so_new_shipp").removeClass("d-none");
