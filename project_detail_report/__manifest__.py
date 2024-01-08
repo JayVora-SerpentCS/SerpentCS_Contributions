058d5c97afe4f57b7d7a2df1b0b0b486744ca656
PratikVaza-SerpentCS <pratik.v.serpentcs@gmail.com>
2024-01-08 17:56:51 +0530

    project_detail_report: Migrated the module.

--- project_detail_report/__manifest__.py
+++ project_detail_report/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Project Report",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Project Management",
     "license": "AGPL-3",
     "summary": """Print Project Detail report with task
