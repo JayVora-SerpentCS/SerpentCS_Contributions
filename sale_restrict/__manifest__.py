8fa1eb3b104ee64cb2376ec80840de791415cc10
PratikVaza-SerpentCS <pratik.v.serpentcs@gmail.com>
2024-01-08 16:29:57 +0530

    [MIG][17.0] sale_restrict: Migrated the module.

--- sale_restrict/__manifest__.py
+++ sale_restrict/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "SO - Product Price Check",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Sales Management",
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "summary": "Restrict sales on 0 value.",
