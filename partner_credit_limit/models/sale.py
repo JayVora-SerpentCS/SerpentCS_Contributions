4e93f27215dc06a1e7da478207315fdebff21c9d
dhaval-serpentcs <dhaval.c.serpentcs@gmail.com>
2024-05-30 12:38:09 +0530

    partner_credit_limit: Migrate partner_credit_limit Module

--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -1,6 +1,5 @@
 # See LICENSE file for full copyright and licensing details.
 
-
 from odoo import _, api, models
 from odoo.exceptions import UserError
 
@@ -10,38 +9,35 @@ class SaleOrder(models.Model):
 
     def check_limit(self):
         self.ensure_one()
+        moveline_obj = self.env['account.move.line']
         partner = self.partner_id
         user_id = self.env['res.users'].search([
             ('partner_id', '=', partner.id)], limit=1)
         if user_id and not user_id.has_group('base.group_portal') or not \
                 user_id:
-            moveline_obj = self.env['account.move.line']
             movelines = moveline_obj.search(
                 [('partner_id', '=', partner.id),
-                 ('account_id.user_type_id.name', 'in',
-                    ['Receivable', 'Payable']),
-                 ('parent_state','!=','cancel')]
+                 ('account_id.account_type', 'in',
+                  ['Receivable', 'Payable']),
+                 ('parent_state', '!=', 'cancel')]
             )
             confirm_sale_order = self.search(
                 [('partner_id', '=', partner.id),
                  ('state', '=', 'sale'),
                  ('invoice_status', '!=', 'invoiced')])
-            debit, credit = 0.0, 0.0
-            amount_total = 0.0
-            for status in confirm_sale_order:
-                amount_total += status.amount_total
-            for line in movelines:
-                credit += line.credit
-                debit += line.debit
-            partner_credit_limit = (
-                debit + amount_total) - credit
+            amount_total = sum(confirm_sale_order.mapped('amount_total'))
+
+            credit = sum(movelines.mapped('credit'))
+            debit = sum(movelines.mapped('debit'))
+
+            partner_credit_limit = (debit + amount_total) - credit
             available_credit_limit = round(
                 partner.credit_limit - partner_credit_limit, 2)
             if partner_credit_limit > partner.credit_limit and \
                     partner.credit_limit > 0.0:
                 if not partner.over_credit:
                     msg = 'Your available credit limit' \
-                          ' Amount = %s \nCheck "%s" Accounts or Credit ' \
+                              ' Amount = %s \nCheck "%s" Accounts or Credit ' \
                           'Limits.' % (available_credit_limit,
                                        self.partner_id.name)
                     raise UserError(_('You can not confirm Sale '
