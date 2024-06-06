f6f6903c3d8460d64a437935b8ead6d6c231f323
Shweta SerpentCS <s.goltar.serpentcs@gmail.com>
2024-06-06 17:01:01 +0530

    project_kanban: Improved help message

--- project_kanban/models/project.py
+++ project_kanban/models/project.py
@@ -12,13 +12,14 @@ class Project(models.Model):
     recent_date = fields.Datetime(
         compute="_compute_get_recent_date",
         string="Recent date",
-        help="This will be auto set",
+        help="This will be automatically set by changing in any task of this project.",
     )
 
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
     def write(self, vals):
+        """This method used for changing project write date."""
         res = super(ProjectTask, self).write(vals)
         if 'write_date' in vals:
             self.project_id.write_date = max(task.write_date for task in self.project_id.task_ids)
