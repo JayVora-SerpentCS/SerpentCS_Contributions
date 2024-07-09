1a351b364941759c1833b84cf9f942241f5bbbec
Shweta SerpentCS <s.goltar.serpentcs@gmail.com>
2024-07-09 13:06:31 +0530

    [MIG] [17.0] project_team : migrated module

--- project_team/models/crm_team.py
+++ project_team/models/crm_team.py
@@ -1,6 +1,7 @@
 # See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+import ast
+from odoo import fields, models, api
 
 
 class CrmTeamInherit(models.Model):
@@ -13,3 +14,24 @@ class CrmTeamInherit(models.Model):
                                         help="""Project's members are users who
                                      can have an access to the tasks related
                                      to this project.""")
+
+class IrModule(models.Model):
+    _inherit = "ir.module.module"
+
+    def remove_action(self, action_data):
+        domain_lst = ast.literal_eval(action_data.domain)
+        return [domain for domain in domain_lst if domain[0] != 'type_team']
+    
+    def module_uninstall(self):
+        action_references = [
+            'sales_team.crm_team_action_sales',
+            'sales_team.crm_team_action_config'
+        ]
+        
+        for ref in action_references:
+            action_data = self.env.ref(ref)
+            if action_data and action_data.domain:
+                action_data.write({'domain': self.remove_action(action_data)})
+    
+        return super().module_uninstall()
+        
