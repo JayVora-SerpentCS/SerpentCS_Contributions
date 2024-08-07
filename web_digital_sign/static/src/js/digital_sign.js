602d7fee8229c7d241d821dd02ad3ca892e463e0
Bhavesh SCS <bhavesh.p.serpentcs@gmail.com>
2024-08-07 18:23:53 +0530

    [MIG] web_digital_sign: Migrated to V16.

--- web_digital_sign/static/src/js/digital_sign.js
+++ web_digital_sign/static/src/js/digital_sign.js
@@ -1,159 +1,126 @@
-odoo.define('web_digital_sign.web_digital_sign', function (require) {
-    "use strict";
+/** @odoo-module */
 
-    var core = require('web.core');
-    var BasicFields= require('web.basic_fields');
-    var FormController = require('web.FormController');
-    var Registry = require('web.field_registry');
-    var utils = require('web.utils');
-    var session = require('web.session');
-    var field_utils = require('web.field_utils');
+import { BinaryField } from "@web/views/fields/binary/binary_field";
+import { registry } from "@web/core/registry";
+import { onWillStart, onMounted, useRef } from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+import { isBinarySize, toBase64Length } from "@web/core/utils/binary";
+import { useService } from "@web/core/utils/hooks";
 
-    var _t = core._t;
-    var QWeb = core.qweb;
+export class FieldSignature extends BinaryField {
 
-    var FieldSignature = BasicFields.FieldBinaryImage.extend( {
-        template: 'FieldSignature',
-        events: _.extend({}, BasicFields.FieldBinaryImage.prototype.events, {
-            'click .save_sign': '_on_save_sign',
-            'click #sign_clean': '_on_clear_sign',
-        }),
-        jsLibs: ['/web_digital_sign/static/lib/jSignature/jSignatureCustom.js'],
-        placeholder: "/web/static/img/placeholder.png",
-        init: function (parent, name, record) {
-            this._super.apply(this, arguments);
-            this.sign_options = {
-                'decor-color': '#D1D0CE',
-                'color': '#000',
-                'background-color': '#fff',
-                'height': '150',
-                'width': '550',
-            };
-            this.empty_sign = [];
-        },
-        start: function () {
-            var self = this;
-            this.$(".signature").jSignature("init", this.sign_options);
-            this.$(".signature").attr( {
-                "tabindex": "0",
-                'height': "100",
+    setup() {
+        super.setup();
+        this.empty_sign = [];
+        this.sign_options = {
+            'decor-color': '#D1D0CE',
+            'color': '#000',
+            'background-color': '#fff',
+            'height': '150',
+            'width': '550',
+        };
+        this.orm = useService("orm");
+        this.drawsign = useRef("drawsign")
+
+        onWillStart(async () => {
+            await loadJS("/web_digital_sign/static/lib/jSignature/jSignatureCustom.js");
+        });
+
+        onMounted(async () => {
+            let $signature = $(this.drawsign.el).find('.signature')
+            this.renderSignature();
+            $signature.jSignature("init", this.sign_options);
+            $signature.change(() => {
+                this.save_sign()
             });
-            this.empty_sign = this.$(".signature").jSignature("getData",
+            this.empty_sign = $signature.jSignature("getData",
                 'image');
-            self._render();
-        },
-        _on_clear_sign: function () {
-            this.$(".signature > canvas").remove();
-            this.$('> img').remove();
-            this.$(".signature").attr("tabindex", "0");
-            var sign_options = {
-                'decor-color': '#D1D0CE',
-                'color': '#000',
-                'background-color': '#fff',
-                'height': '150',
-                'width': '550',
-                'clear': true,
+        })
+    }
+
+    async renderSignature() {
+        var url = this.props.placeholder;
+        if (this.props.record.data.signature && !isBinarySize(this.props.record.data[this.props.name])) {
+            url = 'data:image/png;base64,' + this.props.record.data[this.props.name]
+        } else if (this.props.record?.data?.signature) {
+            this.field = {
+                model: this.props.record.resModel,
+                id: this.props.record.resId,
+                field: this.props.name,
+                filename_field: this.fileName,
+                filename: this.fileName || "",
+                data: isBinarySize(this.props.record.data[this.props.name])
+                    ? null
+                    : this.props.record.data[this.props.name],
             };
-            this.$(".signature").jSignature(sign_options);
-            this.$(".signature").focus();
-            this._setValue(false);
-        },
-        _on_save_sign: function (value_) {
-            var self = this;
-            this.$('> img').remove();
-            var signature = this.$(".signature").jSignature("getData", 'image');
-            var is_empty = signature ?
-                self.empty_sign[1] === signature[1] :
-                false;
-            if (!is_empty && typeof signature !== "undefined" && signature[1]) {
-                this._setValue(signature[1]);
-            }
-        },
-        _render: function () {
-            var self = this;
-            var url = this.placeholder;
-            if (this.value && !utils.is_bin_size(this.value)) {
-                url = 'data:image/png;base64,' + this.value;
-            } else if (this.value) {
-                url = session.url('/web/image', {
-                    model: this.model,
-                    id: JSON.stringify(this.res_id),
-                    field: this.nodeOptions.preview_image || this.name,
-                    unique:
-                    field_utils.format.datetime(
-                        this.recordData.__last_update).replace(/[^0-9]/g, ''),
-                });
+        } else {
+            url = this.placeholder;
+        }
+        if (!this.props.readonly) {
+            $('> img').remove();
+            if (this.props.record.data.signature) {
+                const binary_image = await this.orm.call(this.field.model, 'read', [this.field.id, [this.field.field]],)
+                if (binary_image) {
+                    self.$(".signature").jSignature("clear");
+                    self.$(".signature").jSignature("setData",
+                        'data:image/png;base64,' + binary_image[0].signature);
+                }
             } else {
-                url = this.placeholder;
+                $('> img').remove();
+                $('.signature > canvas').remove();
+                var sign_options = {
+                    'decor-color': '#D1D0CE',
+                    'color': '#000',
+                    'background-color': '#fff',
+                    'height': '150',
+                    'width': '550',
+                };
             }
-            if (this.mode === "readonly") {
-                var $img = $(QWeb.render("FieldBinaryImage-img", {
-                    widget: self,
-                    url: url,
-                }));
-                this.$('> img').remove();
-                this.$(".signature").hide();
-                this.$el.prepend($img);
-                $img.on('error', function () {
-                    self._clearFile();
-                    $img.attr('src', self.placeholder);
-                    self.displayNotification({message:_t("Image Could not display the selected image."),type: 'danger'});
+        } else if (this.mode === 'create') {
+            $('> img').remove();
+            $('> canvas').remove();
+            if (!this.value) {
+                $(".signature").empty().jSignature("init", {
+                    'decor-color': '#D1D0CE',
+                    'color': '#000',
+                    'background-color': '#fff',
+                    'height': '150',
+                    'width': '550',
                 });
-            } else if (this.mode === "edit") {
-                this.$('> img').remove();
-                if (this.value) {
-                    var field_name = this.nodeOptions.preview_image ?
-                        this.nodeOptions.preview_image :
-                        this.name;
-                    self._rpc( {
-                        model: this.model,
-                        method: 'read',
-                        args: [this.res_id, [field_name]],
-                    }).then(function (data) {
-                        if (data) {
-                            var field_desc = _.values(_.pick(data[0],
-                                field_name));
-                            self.$(".signature").jSignature("clear");
-                            self.$(".signature").jSignature("setData",
-                                'data:image/png;base64,' + field_desc[0]);
-                        }
-                    });
-                } else {
-                    this.$('> img').remove();
-                    this.$('.signature > canvas').remove();
-                    var sign_options = {
-                        'decor-color': '#D1D0CE',
-                        'color': '#000',
-                        'background-color': '#fff',
-                        'height': '150',
-                        'width': '550',
-                    };
-                    this.$(".signature").jSignature("init", sign_options);
-                }
-            } else if (this.mode === 'create') {
-                this.$('> img').remove();
-                this.$('> canvas').remove();
-                if (!this.value) {
-                    this.$(".signature").empty().jSignature("init", {
-                        'decor-color': '#D1D0CE',
-                        'color': '#000',
-                        'background-color': '#fff',
-                        'height': '150',
-                        'width': '550',
-                    });
-                }
             }
-        },
-    });
+        }
+    }
 
-    FormController.include( {
-        saveRecord: function () {
-            this.$('.save_sign').click();
-            return this._super.apply(this, arguments);
-        },
-    });
+    save_sign() {
+        var self = this;
+        $('> img').remove();
+        var signature = $(".signature").jSignature("getData", 'image');
+        var is_empty = signature ?
+            self.empty_sign[1] === signature[1] :
+            false;
+        if (!is_empty && typeof signature !== "undefined" && signature[1]) {
+            const changes = { [this.props.name]: signature[1] || false };
+            this.props.record.update(changes);
+        }
+    }
 
-    Registry.add('signature', FieldSignature);
+    sign_clean() {
+        $(".signature > canvas").remove();
+        $('> img').remove();
+        $(".signature").attr("tabindex", "0");
+        var sign_options = {
+            'decor-color': '#D1D0CE',
+            'color': '#000',
+            'background-color': '#fff',
+            'height': '150',
+            'width': '550',
+            'clear': true,
+        };
+        $(".signature").jSignature(sign_options);
+        $(".signature").focus();
+    }
 
+}
+FieldSignature.template = "web_digital_sign.FieldSignature";
 
-});
+registry.category("fields").add("digital_signature", FieldSignature);
