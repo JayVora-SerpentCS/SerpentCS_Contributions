8a67a28d353ed666f120aa6e02ab56675de854a5
ShwetaSerpentCS <s.goltar.serpentcs@gmail.com>
2025-06-10 15:45:30 +0530

    [IMP] [17.0] sale_restrict: improved code

--- sale_restrict/models/sale_order.py
+++ sale_restrict/models/sale_order.py
@@ -13,25 +13,23 @@ class SaleOrder(models.Model):
         zero_price_products = [
             line.product_id.name for line in self.order_line if line.price_unit <= 0.0
         ]
-        is_zero_subtotal =  [
+        is_zero_subtotal = [
             line.product_id.name for line in self.order_line if line.price_subtotal <= 0.0
         ]
-        is_zero_total = self.amount_total <= 0.0
 
         if self.env.user.has_group('sale_restrict.group_sales_restrict_user_validation'):
             if zero_price_products:
-                message = "Please specify unit price for the following products:\n"
-                for product in zero_price_products:
-                    message += product + "\n"
+                message = "Please specify unit price for the following products:\n" + \
+                        "\n".join(zero_price_products)
                 raise UserError(message)
-            
+
             if is_zero_subtotal:
-                message = "The following products have a zero subtotal:\n"
-                for product in is_zero_subtotal:
-                    message += product + "\n"
+                message = "The following products have a zero subtotal:\n" + \
+                        "\n".join(is_zero_subtotal)
                 raise UserError(message)
-                
-            if is_zero_total:
+
+            if self.amount_total <= 0.0:
                 raise UserError("The total amount of the sale order cannot be zero.")
 
         return super(SaleOrder, self).action_confirm()
+
