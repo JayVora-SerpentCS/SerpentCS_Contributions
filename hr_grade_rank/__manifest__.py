4633d1ba897eb94c9a0f8d3d1724dc755d87128c
ShwetaSerpentCS <108718213+ShwetaSerpentCS@users.noreply.github.com>
2025-09-19 18:14:02 +0530

    [MIG] [19.0] hr_grade_rank: migrated module (#679)
    * Added hr_grade_rank module to allow manage your HR with box system
    
    * Fixed Travis.
    
    * Improved Code in website_product_brand module and fixed travis.
    
    * Added Test Case.
    
    * Travis Fixed.
    
    [IMP]Improved code in js.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.
    
    [IMP]Js Travis Fix.
    
    * added banner
    
    * added banners
    
    * fixed summary
    
    * migrated modules and changed index files.
    
    * Migrated modules(hr_grade_rank, partner_credit_limit, sale_cancel) from v10 to v11
    
    * Update __manifest__.py
    
    * made improvements, fix travis errors
    
    * add solve travis error
    
    * improvements in js files and added redme.rst file
    
    * removed encoding utf-8 from py files
    
    * remove description key in manifest file and improvements in js file
    
    * improvements in manifest file
    
    * Fixed travis error and added README.rst file.
    
    * solve travis
    
    * made improvements in redme.rst files
    
    * hr_grade_rank:improve index file
    
    * hr_grade_rank:Module hr grade ranke v12
    
    * hr_grade_rank:Module hr grade ranke v12
    
    * hr_grade_rank:Module hr grade ranke v12
    
    * Module migrated in v13.
    
    * hr_grade_rank : Fixed the travis issue.
    
    * Migrated the module in v14.
    
    * hr_grade_rank: Fix the comments
    
    * hr_grade_rank
    
    * Label Migration comments updated
    
    * updated defect sheet issues and suggestions
    
    * defect sheet issue fixed
    
    * hr_grade_rank: Migrated the module.
    
    * hr_grade_rank: Migrated the module.
    
    * hr_grade_rank: improved icon & banner
    
    * hr_grade_rank: migrated module
    
    * hr_grade_rank: improved banner & icon
    
    * hr_grade_rank: improved code
    
    * hr_grade_rank: improved index file
    
    * [MIG] [19.0] hr_grade_rank: migrated module
    
    ---------
    
    Co-authored-by: Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
    Co-authored-by: Hemangini Patel <h.patel.serpentcs@gmail.com>
    Co-authored-by: Husen <husen.daudi@serpentcs.com>
    Co-authored-by: Vijay Dhorajiya <vijay.dhorajiya.serpentcs@gmail.com>
    Co-authored-by: Jagdish Panchal <jagdish.panchal@serpentcs.com>
    Co-authored-by: Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
    Co-authored-by: arpit.joshi.serpentcs@gmail.com <arpit.joshi.serpentcs@gmail.com>
    Co-authored-by: Shobha Padasalagi <shobha.p.serpentcs@gmail.com>
    Co-authored-by: Nisha Gajjar <n.gajjar.serpentcs@gmail.com>
    Co-authored-by: Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
    Co-authored-by: RiddhiChavda22 <riddhi.odoo@gmail.com>
    Co-authored-by: Pragati Patel <pragatipp98@gmail.com>
    Co-authored-by: ilakkia-serpentcs <ila.d.serpentcs@gmail.com>
    Co-authored-by: PratikVaza-SerpentCS <pratik.v.serpentcs@gmail.com>

---
+++ hr_grade_rank/__manifest__.py
@@ -0,0 +1,20 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "HR-Grade Rank",
+    "version": "19.0",
+    "license": "AGPL-3",
+    "summary": "Manage grade and rank of employee",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "maintainer": "Serpent Consulting Services Pvt. Ltd.",
+    "category": "Human Resources",
+    "website": "https://www.serpentcs.com",
+    "depends": ["hr"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/hr_grade_rank_view.xml"
+    ],
+    "images": ["static/description/EMPLOYEE_GRADE_AND_RANK_MANAGEMENT.jpg"],
+    "sequence": 1,
+    "installable": True,
+}
