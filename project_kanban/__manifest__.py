9bd4c4f4b26e8a7a65176e26057f934dbe20affb
RaishkhanSerpentCS <raish.p.serpentcs@gmail.com>
2026-01-01 19:10:25 +0530

    [MIG][19.0] Migrated module project_kanban in v18.

--- project_kanban/__manifest__.py
+++ project_kanban/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Project - Status by Recent Activities",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "license": "LGPL-3",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
@@ -13,6 +13,6 @@
                 for the Project.""",
     "depends": ["project"],
     "data": ["views/project_kanban_view.xml"],
-    "images": ["static/description/Banner_project_kanban.png"],
+    "images": ["static/description/project_status_by_recent_activities_product_banner.jpg"],
     "installable": True,
 }
