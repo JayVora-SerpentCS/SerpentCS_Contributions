6e563529c47d75d2a71e31dc299e88bee1537b3b
kishan-serpentcs <kishan.j.serpentcs@gmail.com>
2026-05-13 14:29:44 +0530

    [IMP][19.0]partner_credit_limit : Improvements based on code review suggestions.

--- partner_credit_limit/models/account_move.py
+++ partner_credit_limit/models/account_move.py
@@ -8,14 +8,10 @@ class AccountMove(models.Model):
             if move.move_type != "out_invoice":
                 continue
 
-            move.with_company(move.company_id)
-            total_field = (
-                "total_amount_currency"
-                if move.currency_id == move.company_currency_id
-                else "total_amount"
-            )
-            current_amount = move.tax_totals[total_field]
-            move.partner_id.commercial_partner_id._check_credit_limit_for_amount(
+            current_amount = move.amount_total
+            move.partner_id.commercial_partner_id.with_company(
+                move.company_id
+            )._check_credit_limit_for_amount(
                 move,
                 current_amount=current_amount,
                 exclude_amount=move._get_partner_credit_warning_exclude_amount(),
--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -7,9 +7,10 @@ class SaleOrder(models.Model):
 
     def check_limit(self):
         self.ensure_one()
-        self.with_company(self.company_id)
-        current_amount = self.amount_total / self.currency_rate if self.currency_rate else self.amount_total
-        self.partner_id.commercial_partner_id._check_credit_limit_for_amount(
+        current_amount = self.amount_total
+        self.partner_id.commercial_partner_id.with_company(
+            self.company_id
+        )._check_credit_limit_for_amount(
             self,
             current_amount=current_amount,
         )
